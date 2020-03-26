ecc3155f4ea5026f06568fe3df5e0c9e75280727
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-03-25 23:32:05 -0700

    Extract heading into its own file

---
+++ src/homepage/Heading.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { Row, Jumbotron, Col } from "reactstrap";
+import { GiCircuitry } from "react-icons/gi"
+import { BsCodeSlash } from "react-icons/bs"
+
+import { HardwareYears, ProgrammingYears } from "./YearsSince";
+
+export function Heading() {
+    return (
+      <Jumbotron fluid>
+        <Col className="text-center">
+          <p className="lead">My name is</p>
+          <h1 className="display-1">ASTRID</h1>
+          <p className="lead">and I'm a</p>
+          <h1 className="display-1">HACKER</h1>
+        </Col>
+        <Row>
+          <Col className="text-center" md={6}>
+            <div className="d-inline-flex">
+              <Col md="auto">
+                <BsCodeSlash style={{fontSize: 100, height: "100%"}}/>
+              </Col>
+              <Col md="auto" className="text-left">
+                <p>I've worked with</p>
+                <h2>SOFTWARE</h2>
+                <p>for <strong><ProgrammingYears/> years</strong></p>
+              </Col>
+            </div>
+          </Col>
+          <Col className="text-center" md={6}>
+            <div className="d-inline-flex">
+              <Col md="auto">
+                <GiCircuitry style={{fontSize: 100, borderColor: "#FFFFFF", borderStyle: "solid"}}/>
+              </Col>
+              <Col md="auto" className="text-left">
+                <p>as well as</p>
+                <h2>HARDWARE</h2>
+                <p>for <strong><HardwareYears/> years.</strong></p>
+              </Col>
+            </div>
+          </Col>
+        </Row>
+      </Jumbotron>
+    )
+}
+
+
+
+
+
--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,49 +1,13 @@
 import React from "react";
-import { Media, Row, Container, Jumbotron, Col } from "reactstrap";
+import { Container } from "reactstrap";
 import "../bootstrap.css";
-import { ProgrammingYears, HardwareYears } from "./YearsSince";
 import { SkillsSection } from "./Skills";
-import imgMyFace from "../assets/my-face.jpg"
-import { GiCircuitry } from "react-icons/gi"
-import { BsCodeSlash } from "react-icons/bs"
+import { Heading } from "./Heading";
 
 function Homepage() {
   return (
     <Container fluid>
-      <Jumbotron fluid>
-        <Col className="text-center">
-          <p className="lead">My name is</p>
-          <h1 className="display-1">ASTRID</h1>
-          <p className="lead">and I'm a</p>
-          <h1 className="display-1">HACKER</h1>
-        </Col>
-        <Row>
-          <Col className="text-center" md={6}>
-            <div className="d-inline-flex">
-              <Col md="auto">
-                <BsCodeSlash style={{fontSize: 100, height: "100%"}}/>
-              </Col>
-              <Col md="auto" className="text-left">
-                <p>I've worked with</p>
-                <h2>SOFTWARE</h2>
-                <p>for <strong><ProgrammingYears/> years</strong></p>
-              </Col>
-            </div>
-          </Col>
-          <Col className="text-center" md={6}>
-            <div className="d-inline-flex">
-              <Col md="auto">
-                <GiCircuitry style={{fontSize: 100, borderColor: "#FFFFFF", borderStyle: "solid"}}/>
-              </Col>
-              <Col md="auto" className="text-left">
-                <p>as well as</p>
-                <h2>HARDWARE</h2>
-                <p>for <strong><HardwareYears/> years.</strong></p>
-              </Col>
-            </div>
-          </Col>
-        </Row>
-      </Jumbotron>
+      <Heading/>
 
       <Container>
         <div className="row">
@@ -55,8 +19,6 @@ function Homepage() {
             </p>
             <article>
               <p>
-                Hey, I'm Astrid! I've been programming
-                for <ProgrammingYears /> years and I've worked with hardware for <HardwareYears /> years. 
               </p>
               <p>
 
@@ -64,9 +26,9 @@ function Homepage() {
             </article>
           </div>
         </div>
-
         <SkillsSection />
       </Container>
+      
       <footer style={{ background: "#202030" }}>
         <div className="row">
           <div className="col-lg-12">
