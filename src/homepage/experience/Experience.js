8ab44c9b07c1e3a2e2f6767d86ae709656c57adc
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-03-28 20:54:23 -0700

    Prettify and make the selector work

--- src/homepage/experience/Experience.js
+++ src/homepage/experience/Experience.js
@@ -13,7 +13,7 @@ import {
   CarouselItem,
   CarouselIndicators,
   CarouselControl,
-  CarouselCaption
+  CarouselCaption,
 } from "reactstrap";
 import { imgIronPanthersWide } from "../../assets";
 import { TimelineInterval, TimelineLayer } from "./timeline";
@@ -24,7 +24,7 @@ function IronPanthersInfoCard() {
       <CardHeader>
         <h4>Iron Panthers Robotics Team</h4>
       </CardHeader>
-      <CardImg src={imgIronPanthersWide} alt="Iron Panthers"/>
+      <CardImg src={imgIronPanthersWide} alt="Iron Panthers" />
       <CardBody>
         <CardText>
           <Badge
@@ -50,33 +50,19 @@ function IronPanthersInfoCard() {
           </Badge>
         </CardText>
         <CardText>
-            On the FIRST Robotics Competition main team, I worked to build a
-            vision processing system using a Nvidia Jetson as a co-processor to
-            aid the driver in aligning the robot.
+          On the FIRST Robotics Competition main team, I worked to build a
+          vision processing system using a Nvidia Jetson as a co-processor to
+          aid the driver in aligning the robot.
         </CardText>
         <CardText>
-            On the FIRST Tech Challenge subteam, I designed code for autonomous
-            operation period. Created a versatile command-based system to better
-            organize the auto code.
+          On the FIRST Tech Challenge subteam, I designed code for autonomous
+          operation period. Created a versatile command-based system to better
+          organize the auto code.
         </CardText>
       </CardBody>
     </Card>
   );
 }
-
-function ExperienceInterval({ start, end, workId, title, color }) {
-  const {selected, setSelected} = useContext(ExperienceContext);
-  const onClick = () => setSelected(workId);
-  return (
-    <TimelineInterval start={start} end={end}>
-      <Button active={selected === workId} onClick={onClick} style={{ width: "100%", height: "100%"}}>
-        {title}
-      </Button>
-    </TimelineInterval>
-  );
-}
-
-const ExperienceContext = createContext(null);
 
 function dateIndex(year, month) {
   return year * 12 + month;
@@ -86,46 +72,72 @@ const WORK_EXPERIENCES = [
   {
     id: "ironPanthers",
     title: "Iron Panthers Robotics Team",
-    info: <IronPanthersInfoCard/>,
+    info: <IronPanthersInfoCard />,
     start: dateIndex(2014, 8),
-    end: dateIndex(2019, 5)
+    end: dateIndex(2019, 5),
   },
   {
     id: "goodRipple",
     title: "GoodRipple Inc.",
-    info: <div><p>WIP</p></div>,
+    info: (
+      <div>
+        <p>WIP</p>
+      </div>
+    ),
     start: dateIndex(2018, 6),
     end: dateIndex(2018, 7),
   },
   {
     id: "fabTime",
     title: "FabTime Inc.",
-    info: <div><p>WIP</p></div>,
+    info: (
+      <div>
+        <p>WIP</p>
+      </div>
+    ),
     start: dateIndex(2019, 11),
-    end: dateIndex(2020, 6)
-  }
-]
+    end: dateIndex(2020, 6),
+  },
+];
 
-function ExperienceCardCarousel({ items }) {
-  const [activeIndex, setActiveIndex] = useState(0);
+function ExperienceInterval({ start, end, thisIndex, title, color }) {
+  const { selected, setSelected } = useContext(ExperienceContext);
+  const onClick = () => setSelected(thisIndex);
+  return (
+    <TimelineInterval start={start} end={end}>
+      <Button
+        active={selected === thisIndex}
+        onClick={onClick}
+        style={{ width: "100%", height: "100%" }}
+      >
+        {title}
+      </Button>
+    </TimelineInterval>
+  );
+}
+
+const ExperienceContext = createContext(null);
+
+function ExperienceCardCarousel() {
+  const { selected, setSelected, items } = useContext(ExperienceContext);
   const [animating, setAnimating] = useState(false);
 
   const next = () => {
     if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  }
+    const nextIndex = selected === items.length - 1 ? 0 : selected + 1;
+    setSelected(nextIndex);
+  };
 
   const previous = () => {
     if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  }
+    const nextIndex = selected === 0 ? items.length - 1 : selected - 1;
+    setSelected(nextIndex);
+  };
 
   const goToIndex = (newIndex) => {
     if (animating) return;
-    setActiveIndex(newIndex);
-  }
+    setSelected(newIndex);
+  };
 
   const slides = items.map((item) => {
     return (
@@ -135,55 +147,81 @@ function ExperienceCardCarousel({ items }) {
         key={item.id}
       >
         {item.info}
-        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        <CarouselCaption
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
       </CarouselItem>
     );
   });
 
   return (
-    <Carousel
-      activeIndex={activeIndex}
-      next={next}
-      previous={previous}
-    >
-      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+    <Carousel activeIndex={selected} next={next} previous={previous}>
+      <CarouselIndicators
+        items={items}
+        activeIndex={selected}
+        onClickHandler={goToIndex}
+      />
       {slides}
-      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
     </Carousel>
   );
 }
 
-function ExperienceTimelineCarousel({items}) {
-  const [selected, setSelected] = useState("ironPanthers");
-  const earliestIndex = Math.min.apply(null, items.map(e => e.start));
+function ExperienceTimelineCarousel({ style, items, initialId }) {
+  items.sort((e) => e.start);
+
+  const [selected, setSelected] = useState(
+    items.find((e) => e.id === initialId)
+  );
+  const earliestIndex = Math.min.apply(
+    null,
+    items.map((e) => e.start)
+  );
 
   return (
-    <ExperienceContext.Provider value={{selected, setSelected}}>
-      <h2>Work experience</h2>
-      <Row>
-        <Col xs={2}>
-          <TimelineLayer>
-            {
-              items.map(e => (
-                <ExperienceInterval start={e.start - earliestIndex} end={e.end - earliestIndex} workId={e.id} title={e.title} color={e.color}/>
-              ))
-            }
-          </TimelineLayer>
-        </Col>
-        <Col>
-          <ExperienceCardCarousel items={items}/>
-        </Col>
-      </Row>
+    <ExperienceContext.Provider value={{ selected, setSelected, items }}>
+      <div style={style}>
+        <h2>Work experience</h2>
+        <Row>
+          <Col xs={2}>
+            <TimelineLayer>
+              {items.map((e, i) => (
+                <ExperienceInterval
+                  start={e.start - earliestIndex}
+                  end={e.end - earliestIndex}
+                  thisIndex={i}
+                  title={e.title}
+                  color={e.color}
+                />
+              ))}
+            </TimelineLayer>
+          </Col>
+          <Col>
+            <ExperienceCardCarousel />
+          </Col>
+        </Row>
+      </div>
     </ExperienceContext.Provider>
-  )
+  );
 }
 
 export function ExperienceSection() {
-
   return (
-    <section>
-      <ExperienceTimelineCarousel items={WORK_EXPERIENCES}/>
+    <section style={{ height: 1000 }}>
+      <ExperienceTimelineCarousel
+        style={{ height: "100%" }}
+        items={WORK_EXPERIENCES}
+      />
     </section>
   );
 }
