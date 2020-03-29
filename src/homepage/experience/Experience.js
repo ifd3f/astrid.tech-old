6216ba997ca59e1412121b6bfdb1266a6380ec0f
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-03-28 20:31:00 -0700

    Carousel

--- src/homepage/experience/Experience.js
+++ src/homepage/experience/Experience.js
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState } from "react";
-import ReactDOM from "react-dom";
 import {
   Badge,
   Card,
@@ -10,13 +9,18 @@ import {
   CardImg,
   Button,
   Col,
+  Carousel,
+  CarouselItem,
+  CarouselIndicators,
+  CarouselControl,
+  CarouselCaption
 } from "reactstrap";
 import { imgIronPanthersWide } from "../../assets";
-import { Timeline, TimelineInterval, TimelineLayer } from "./timeline";
+import { TimelineInterval, TimelineLayer } from "./timeline";
 
 function IronPanthersInfoCard() {
   return (
-    <Card style={{maxWidth: 500}}>
+    <Card>
       <CardHeader>
         <h4>Iron Panthers Robotics Team</h4>
       </CardHeader>
@@ -72,29 +76,114 @@ function ExperienceInterval({ start, end, workId, title, color }) {
   );
 }
 
-const ExperienceContext = createContext({
-  selected: null,
-});
+const ExperienceContext = createContext(null);
+
+function dateIndex(year, month) {
+  return year * 12 + month;
+}
+
+const WORK_EXPERIENCES = [
+  {
+    id: "ironPanthers",
+    title: "Iron Panthers Robotics Team",
+    info: <IronPanthersInfoCard/>,
+    start: dateIndex(2014, 8),
+    end: dateIndex(2019, 5)
+  },
+  {
+    id: "goodRipple",
+    title: "GoodRipple Inc.",
+    info: <div><p>WIP</p></div>,
+    start: dateIndex(2018, 6),
+    end: dateIndex(2018, 7),
+  },
+  {
+    id: "fabTime",
+    title: "FabTime Inc.",
+    info: <div><p>WIP</p></div>,
+    start: dateIndex(2019, 11),
+    end: dateIndex(2020, 6)
+  }
+]
+
+function ExperienceCardCarousel({ items }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.id}
+      >
+        {item.info}
+        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+}
+
+function ExperienceTimelineCarousel({items}) {
+  const [selected, setSelected] = useState("ironPanthers");
+  const earliestIndex = Math.min.apply(null, items.map(e => e.start));
+
+  return (
+    <ExperienceContext.Provider value={{selected, setSelected}}>
+      <h2>Work experience</h2>
+      <Row>
+        <Col xs={2}>
+          <TimelineLayer>
+            {
+              items.map(e => (
+                <ExperienceInterval start={e.start - earliestIndex} end={e.end - earliestIndex} workId={e.id} title={e.title} color={e.color}/>
+              ))
+            }
+          </TimelineLayer>
+        </Col>
+        <Col>
+          <ExperienceCardCarousel items={items}/>
+        </Col>
+      </Row>
+    </ExperienceContext.Provider>
+  )
+}
 
 export function ExperienceSection() {
-  const [selected, setSelected] = useState("tii");
 
   return (
     <section>
-      <ExperienceContext.Provider value={{selected, setSelected}}>
-        <h2>Work experience</h2>
-        <Row>
-          <Col xs={2}>
-          <TimelineLayer>
-            <ExperienceInterval start={0} end={5} workId="foo" title="tiii" color="red"/>
-            <ExperienceInterval start={1} end={4} workId="fffoo" title="tii" color="red"/>
-          </TimelineLayer>
-          </Col>
-          <Col>
-          <IronPanthersInfoCard />
-          </Col>
-        </Row>
-      </ExperienceContext.Provider>
+      <ExperienceTimelineCarousel items={WORK_EXPERIENCES}/>
     </section>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "./bootstrap.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
