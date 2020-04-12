ad3430578249085f3395da8dceba7f20f01750bc
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-04-12 13:11:13 -0700

    404 page

---
+++ src/404.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Container, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+
+export default function Page404() {
+  return (
+    <Container>
+      <h2>404 - There's nothing here!</h2>
+      <p>
+        Your link seems to be broken, but your code certainly wouldn't be if I
+        was on your team!
+      </p>
+      <Button tag={Link} to="/">
+        Find out more about my work
+      </Button>
+      <p>
+        Unless my website directed you here, of course. In that case, please
+        tell me what went wrong at{" "}
+        <a href="mailto:astridyu3.14@gmail.com">astridyu3.14@gmail.com</a>.
+      </p>
+      <h3>Where can you go?</h3>
+      <ul>
+        <li>
+          <Link to="/">The homepage</Link> where you can see my face and some
+          words about me
+        </li>
+        <li>
+          <Link to="/blog">My blog</Link> where you can see the words in my head
+        </li>
+        <li>
+          <Link to="/things">A directory of things I've done</Link>
+        </li>
+      </ul>
+    </Container>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import "./bootstrap.scss";
 import Homepage from "./homepage/Homepage";
 import MainNavbar from "./mainnavbar/MainNavbar";
+import Page404 from "./404";
 
 function App() {
   return (
@@ -13,8 +14,14 @@ function App() {
       <MainNavbar />
       <div style={{ paddingTop: 70 }}>
         <Switch>
-          <Route path="/">
+          <Route path="/" exact>
             <Homepage />
+          </Route>
+          <Route path="/404" exact>
+            <Page404 />
+          </Route>
+          <Route path="*">
+            <Redirect to="/404" />
           </Route>
         </Switch>
       </div>
