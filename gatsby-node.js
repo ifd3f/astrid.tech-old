01cff6642442c663b11b13a2ccce1c35ebd3218d
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-06-09 17:22:42 -0700

    Project node stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,54 @@ const mkdirp = require("mkdirp")
 const md5 = require("js-md5")
 const yaml = require("js-yaml")
 
+const SLUG_OVERRIDE = {
+  "c++": "cpp",
+  "c#": "csharp",
+  "f#": "fsharp",
+  "objective-c++": "objective-cpp",
+}
+
+const getTagSlug = name => {
+  const lower = name.toLowerCase()
+  return SLUG_OVERRIDE[lower] || lower.replace(" ", "-")
+}
+
+const setContentDigest = node => {
+  // Get content digest of node. (Required field)
+  const contentDigest = crypto
+    .createHash(`md5`)
+    .update(JSON.stringify(node))
+    .digest(`hex`)
+  // add it to userNode
+  node.internal.contentDigest = contentDigest
+}
+
+const buildTagNode = ({ name, slug, color, textColor }) => {
+  const node = {
+    parent: `__SOURCE__`,
+    internal: {
+      type: `Tag`,
+    },
+    children: [],
+
+    id: uuid.v4(),
+    name,
+    slug,
+    color,
+    textColor,
+  }
+
+  // Get content digest of node. (Required field)
+  const contentDigest = crypto
+    .createHash(`md5`)
+    .update(JSON.stringify(node))
+    .digest(`hex`)
+  // add it to userNode
+  node.internal.contentDigest = contentDigest
+
+  return node
+}
+
 const createBlogPosts = async ({ graphql, actions }) => {
   const { createPage } = actions
 
@@ -57,109 +105,18 @@ const createBlogPosts = async ({ graphql, actions }) => {
   })
 }
 
-const createProjects = async ({ graphql, actions }) => {
-  const { createPage, createNode } = actions
-
-  const ProjectDetail = path.resolve(`./src/templates/project-detail.tsx`)
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        filter: { frontmatter: { type: { eq: "project" } } }
-        sort: { fields: [frontmatter___date], order: DESC }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              tags
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  if (result.errors) {
-    throw result.errors
-  }
-
-  const projects = result.data.allMarkdownRemark.edges
-
-  projects.forEach(edge => {
-    const project = edge.node
-
-    const tagNode = buildTagNode({
-      name: project.frontmatter.title,
-      slug: project.fields.slug,
-      color: "#e81272",
-      textColor: "#e0c23e",
-    })
-
-    createNode(tagNode)
-
-    createPage({
-      path: project.fields.slug,
-      component: ProjectDetail,
-      context: {
-        slug: project.fields.slug,
-      },
-    })
-  })
-}
-
 exports.onPreBootstrap = () => {
   rimraf.sync(path.resolve(`${__dirname}/static/generated`))
 }
 
-const SLUG_OVERRIDE = {
-  "c++": "cpp",
-  "c#": "csharp",
-  "f#": "fsharp",
-  "objective-c++": "objective-cpp",
-}
-
-const getTagSlug = name => {
-  const lower = name.toLowerCase()
-  return SLUG_OVERRIDE[lower] || lower.replace(" ", "-")
-}
-
-const buildTagNode = ({ name, slug, color, textColor }) => {
-  const node = {
-    parent: `__SOURCE__`,
-    internal: {
-      type: `Tag`,
-    },
-    children: [],
-
-    id: uuid.v4(),
-    name,
-    slug,
-    color,
-    textColor,
-  }
-
-  // Get content digest of node. (Required field)
-  const contentDigest = crypto
-    .createHash(`md5`)
-    .update(JSON.stringify(node))
-    .digest(`hex`)
-  // add it to userNode
-  node.internal.contentDigest = contentDigest
-
-  return node
-}
-
-exports.sourceNodes = async ({ actions }) => {
+const sourceLangTagNodes = async ({ actions }) => {
   const { createNode } = actions
 
   const res = await axios.get(
     "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
   )
 
+  const tagTable = {}
   const langs = yaml.load(res.data)
   for (let key in langs) {
     if (!langs.hasOwnProperty(key)) {
@@ -182,18 +139,179 @@ exports.sourceNodes = async ({ actions }) => {
 
     const node = buildTagNode({
       name: key,
-      slug: "/tags/" + getTagSlug(key),
+      slug: getTagSlug(key),
       color: lang.color,
       textColor,
     })
+    tagTable[node.slug] = node.id
     // Create node with the gatsby createNode() API
     createNode(node)
   }
+
+  return tagTable
+}
+
+const sourceWorkExperienceNodes = async ({ graphql, actions }) => {
+  const { createNode } = actions
+  const rawQueryResult = await graphql(`
+    {
+      allWorkExperienceYaml {
+        edges {
+          node {
+            parent {
+              ... on File {
+                name
+              }
+            }
+            organization
+            position
+            location
+            startDate
+            endDate
+
+            summary
+            website
+
+            highlights
+            tags
+          }
+        }
+      }
+    }
+  `)
+
+  rawQueryResult.edges.forEach(edge => {
+    const rawNode = edge.node
+
+    const node = {
+      parent: `__SOURCE__`,
+      internal: {
+        type: `WorkExperience`,
+      },
+      id: uuid.v4(),
+      children: [],
+
+      slug: "/experience/" + rawNode.parent.name,
+      organization: rawNode.organization,
+      position: rawNode.position,
+      location: rawNode.location,
+      startDate: rawNode.startDate,
+      endDate: rawNode.endDate,
+
+      summary: rawNode.summary,
+      website: rawNode.website,
+
+      highlights: rawNode.highlights,
+      ["tags__NODE"]: rawNode.tags,
+    }
+
+    setContentDigest(node)
+
+    createNode(
+      buildTagNode({
+        name: `${node.position} at ${node.organization}`,
+        slug: node.slug,
+        color: "#169bf4",
+        textColor: "#16f4de",
+      })
+    )
+
+    createNode(node)
+  })
+}
+
+const sourceProjectNodes = async ({ graphql, actions }) => {
+  const { createNode } = actions
+
+  const rawMarkdownQuery = await graphql(`
+    {
+      allMarkdownRemark(
+        filter: { frontmatter: { type: { eq: "project" } } }
+        sort: { fields: [frontmatter___date], order: DESC }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            markdownId: id
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              tags
+              status
+              description
+              startDate
+              endDate
+              url
+              source
+              thumbnail
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  rawMarkdownQuery.data.allMarkdownRemark.edges.forEach(edge => {
+    const project = edge.node
+
+    const projectNode = {
+      parent: `__SOURCE__`,
+      internal: {
+        type: `WorkExperience`,
+      },
+      id: uuid.v4(),
+      children: [],
+
+      slug: project.fields.slug,
+      title: project.frontmatter.title,
+      tags: project.frontmatter.tags,
+      status: project.frontmatter.status,
+      description: project.frontmatter.description,
+      startDate: project.frontmatter.startDate,
+      endDate: project.frontmatter.endDate,
+      url: project.frontmatter.url,
+      source: project.frontmatter.source,
+      thumbnail: project.frontmatter.thumbnail,
+      ["markdown__NODE"]: project.markdownId,
+    }
+
+    setContentDigest(projectNode)
+    createNode(projectNode)
+
+    const tagNode = buildTagNode({
+      name: project.frontmatter.title,
+      slug: project.fields.slug,
+      color: "#e81272",
+      textColor: "#e0c23e",
+    })
+
+    createNode(tagNode)
+  })
+}
+
+const createProjectPages = async () => {
+  const ProjectDetail = path.resolve(`./src/templates/project-detail.tsx`)
+
+  createPage({
+    path: project.fields.slug,
+    component: ProjectDetail,
+    context: {
+      slug: project.fields.slug,
+    },
+  })
+}
+
+exports.sourceNodes = async ({ graphql, actions }) => {
+  const tagTable = sourceLangTagNodes({ graphql, actions })
+  sourceWorkExperienceNodes({ tagTable, graphql, actions })
+  sourceProjectNodes({ tagTable, graphql, actions })
 }
 
 exports.createPages = async ({ graphql, actions }) => {
-  createBlogPosts({ graphql, actions })
-  createProjects({ graphql, actions })
+  //createBlogPosts({ graphql, actions })
+  createProjectPages({ graphql, actions })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
@@ -236,8 +354,4 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value: thumbnailPublicPath,
     })
   }
-}
-
-exports.onPostBuild = (pages, callback) => {
-  callback()
 }
