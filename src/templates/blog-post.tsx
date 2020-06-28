676e1b351b30fff1cc4753850b93dabbacd40842
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-06-27 19:35:05 -0700

    Fix katex not being rendered properly

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -4,6 +4,8 @@ import { PostContent, PostMainHeader, PostSEO } from "../components/blog"
 import Layout from "../components/layout"
 import { BlogPost } from "../types/index"
 import { Container } from "reactstrap"
+import styleBlog from "../scss/blog.module.scss"
+import Helmet from "react-helmet"
 
 export const pageQuery = graphql`
   query BlogPostBySlug($id: String!) {
@@ -56,9 +58,24 @@ const BlogPostTemplate: FC<PageProps<Data, Context>> = ({
 
   return (
     <Layout>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
+          integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq"
+          crossorigin="anonymous"
+        />
+
+        <script
+          defer
+          src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js"
+          integrity="sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz"
+          crossorigin="anonymous"
+        ></script>
+      </Helmet>
       <PostSEO post={post} />
 
-      <Container>
+      <Container className={styleBlog.blogContentContainer}>
         <article>
           <PostMainHeader post={post} />
           <PostContent post={post} />
