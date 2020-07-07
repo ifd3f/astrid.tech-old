05408dcdc39e0cff666c9cddc7bc24262c0fd9f9
Astrid Yu <astridyu3.14@gmail.com>
2020-07-06 23:02:15 -0700

    Fix license to say CC-BY-NC-SA, and xmlns stuff

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -22,23 +22,23 @@ export const Witch = () => {
 
 const ContentLicense = () => (
   <p>
-    <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
       <img
         alt="Creative Commons License"
         style={{ borderWidth: 0 }}
-        src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"
+        src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"
       />
     </a>
     <br />
-    <a
-      href="http://purl.org/dc/dcmitype/InteractiveResource"
+    <span
+      {...{ "xmlns:dct": "http://purl.org/dc/terms/" }}
       property="dct:title"
-      rel="dct:type"
     >
       The page content of astrid.tech
-    </a>{" "}
+    </span>{" "}
     by{" "}
     <a
+      {...{ "xmlns:cc": "http://creativecommons.org/ns#" }}
       href="https://astrid.tech"
       property="cc:attributionName"
       rel="cc:attributionURL"
@@ -46,8 +46,9 @@ const ContentLicense = () => (
       Astrid Yu
     </a>{" "}
     is licensed under a{" "}
-    <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
-      Creative Commons Attribution-ShareAlike 4.0 International License
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+      Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+      License
     </a>
     .
   </p>
@@ -60,7 +61,10 @@ const AGPL: FC<{ logo: any }> = ({ logo }) => {
         <Img fixed={logo} alt="GNU Affero General Public License" />
       </a>
       <p>
-        The source code of the astrid.tech is licensed under the{" "}
+        <a href="https://github.com/plenglin/astrid.tech">
+          The source code of astrid.tech
+        </a>{" "}
+        is licensed under the{" "}
         <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">AGPL License</a>
         .{" "}
       </p>
