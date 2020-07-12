c61de76cfcf8c91c218e60d283655bd272d1c6f2
Astrid Yu <astridyu3.14@gmail.com>
2020-07-11 19:30:24 -0700

    Make notebook posts work again

--- plugins/gatsby-astrid-transformer-notebook-post/gatsby-node.ts
+++ plugins/gatsby-astrid-transformer-notebook-post/gatsby-node.ts
@@ -1,7 +1,9 @@
 import { GatsbyNode, Node } from "gatsby"
-import path from "path"
+import { createFilePath, FileSystemNode } from "gatsby-source-filesystem"
 import { Notebook } from "src/types/nbformat-v4"
 import { v4 } from "uuid"
+import { BlogPostContent } from "../gatsby-astrid-plugin-blog"
+import { BLOG_POST_MIME_TYPE } from "../gatsby-astrid-plugin-blog/index"
 import { withContentDigest } from "../util"
 
 type JupyterNotebookNode = Node & {
@@ -15,6 +17,7 @@ type JupyterNotebookNode = Node & {
       blog_data: BlogMetadata
     }
   }
+  html: string
 }
 
 type BlogMetadata = {
@@ -27,29 +30,38 @@ type BlogMetadata = {
 export const onCreateNode: GatsbyNode["onCreateNode"] = async ({
   node,
   actions,
+  getNode,
   loadNodeContent,
 }) => {
   if (node.internal.type != "ipynb") return
-
   const jupyterNode = (node as unknown) as JupyterNotebookNode
+
+  const parentFileSystem = getNode(jupyterNode.parent) as FileSystemNode
+  if (parentFileSystem.sourceInstanceName != "blog") return
+
   const { createNode, createParentChildLink } = actions
   const { title, date, description, tags } = jupyterNode.data.metadata.blog_data
 
-  const slugBase = path.parse(path.dirname(jupyterNode.fileAbsolutePath)).name
+  const slug = createFilePath({ node, getNode })
+
+  const content: BlogPostContent = {
+    slug,
+    title,
+    date,
+    description,
+    content: await loadNodeContent(jupyterNode),
+    tagSlugs: tags,
+  }
 
   const postNode = (withContentDigest({
     parent: jupyterNode.id,
     internal: {
-      type: "BlogPost",
-      //content: jupyterNode.internal.content,
+      type: "JupyterBlogPost",
+      mediaType: BLOG_POST_MIME_TYPE,
+      content: JSON.stringify(content),
     },
     id: v4(),
     children: [],
-    slug: `/blog/${slugBase}/`,
-    title,
-    date,
-    description,
-    tagSlugs: tags,
   }) as unknown) as Node
 
   createNode(postNode)
