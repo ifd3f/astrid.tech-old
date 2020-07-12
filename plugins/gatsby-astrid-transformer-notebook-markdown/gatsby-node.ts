180d065f5c3c23b0490dfe017a74aba989ee1552
Astrid Yu <astridyu3.14@gmail.com>
2020-07-12 16:04:56 -0700

    Somewhat primitive ipynb -> md conversion but it works

--- plugins/gatsby-astrid-transformer-notebook-markdown/gatsby-node.ts
+++ plugins/gatsby-astrid-transformer-notebook-markdown/gatsby-node.ts
@@ -1,23 +1,57 @@
 import { GatsbyNode, Node } from "gatsby"
-import { createFilePath, FileSystemNode } from "gatsby-source-filesystem"
-import { Notebook } from "src/types/nbformat-v4"
-import { v4 } from "uuid"
-import { BlogPostContent } from "../gatsby-astrid-plugin-blog"
-import { BLOG_POST_MIME_TYPE } from "../gatsby-astrid-plugin-blog/index"
-import { withContentDigest } from "../util"
+import { FileSystemNode } from "gatsby-source-filesystem"
 import { BlogMetadata } from "plugins/gatsby-astrid-transformer-markdown-post"
+import { v4 } from "uuid"
+import { Notebook, CodeCell, Output } from "../../src/types/nbformat-v4"
+import { withContentDigest } from "../util"
 
 type JupyterNotebookNode = Node & {
-  json: Notebook
   internal: {
     content: string
   }
   fileAbsolutePath: string
-  data: {
+  data: Notebook & {
     metadata: {
       blog_data: BlogMetadata
     }
   }
+}
+
+function convertOutputsToMarkdown(output: Output) {
+  switch (output.output_type) {
+    case "stream":
+      return "```" + output.text + "```"
+    case "display_data":
+      return `<img src="data:image/png;base64, ${output.data["image/png"]}" alt="${output.data["text/plain"]}" />`
+  }
+}
+
+function convertCodeCellToMarkdown(lang: string, cell: CodeCell) {
+  const code =
+    "```" + lang + "\n" + convertSourceToString(cell.source) + "\n```"
+  const outputs = cell.outputs.map(convertOutputsToMarkdown)
+
+  return code + "\n\n" + outputs.join("\n\n")
+}
+
+function convertSourceToString(source: string | string[]) {
+  return Array.isArray(source) ? source.join("") : source
+}
+
+function convertNotebookToMarkdown(notebook: Notebook): string {
+  const convertedCells = notebook.cells.map(cell => {
+    switch (cell.cell_type) {
+      case "markdown":
+        return convertSourceToString(cell.source)
+      case "code":
+        return convertCodeCellToMarkdown(
+          notebook.metadata.language_info?.name ?? "",
+          cell
+        )
+    }
+  })
+
+  return convertedCells.join("\n\n")
 }
 
 export const onCreateNode: GatsbyNode["onCreateNode"] = async ({
@@ -37,12 +71,11 @@ export const onCreateNode: GatsbyNode["onCreateNode"] = async ({
   const id = v4()
 
   const frontmatter = jupyterNode.data.metadata.blog_data
-  const content = jupyterNode.internal.content
 
   const markdown = `---
 ${JSON.stringify(frontmatter)}
 ---
-${content}
+${convertNotebookToMarkdown(jupyterNode.data)}
 `
 
   const postNode = (withContentDigest({
