398ab2f6df6a5e5625bf5f88fbdfe514099feaa9
Astrid Yu <astrid@astrid.tech>
2020-12-09 13:16:58 -0800

    OAuth and login

--- astrid_tech_api/astrid_tech/settings.py
+++ astrid_tech_api/astrid_tech/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'oauth2_provider',
     'webmention',
 
     'blog',
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'astrid_tech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +127,9 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
     ]
 }
 
