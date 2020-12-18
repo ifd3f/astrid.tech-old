912fbc3f8f98d3a4fd0ae6642253e43e9b4d3b6a
Astrid Yu <astrid@astrid.tech>
2020-12-18 10:58:29 -0800

    Maximum image size of 1600x1600

--- massresize.sh
+++ massresize.sh
@@ -1,5 +1,5 @@
 maxsize () {
-  convert $1 -resize 2000x2000\> $1
+  convert $1 -resize 1600x1600\> $1
 }
 
 for file in $(find content assets -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) )
