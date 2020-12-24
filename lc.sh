df391bdb08181893013c360ad24856c80ff22eb3
Astrid Yu <astrid@astrid.tech>
2020-12-24 00:25:37 -0800

    Squash migrations, count lines
    8k lines!

--- lc.sh
+++ lc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -type f \( -name '*.tsx' -or -name '*.ts' -or -name '*.jsx' -or -name '*.js' -or -name '*.scss' \) \
+find . -type f \( -name '*.tsx' -or -name '*.ts' -or -name '*.jsx' -or -name '*.js' -or -name '*.scss' -or -name '*.py' \) \
   -not -path "./node_modules/*" -not -path "./content/*"  -not -path "./.cache/*" | \
   xargs wc -l
