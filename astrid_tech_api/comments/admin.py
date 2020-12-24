07138ac84f1fe67ba7e228ca5333268df484a6b5
Astrid Yu <astrid@astrid.tech>
2020-12-24 13:50:16 -0800

    Comment admin

--- astrid_tech_api/comments/admin.py
+++ astrid_tech_api/comments/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.db.models import QuerySet
 
-from comments.models import Comment
+from comments.models import Comment, Report, BannedEmail, BannedIP
 
-admin.register(Comment)
+
+class CommentAdmin(ModelAdmin):
+    actions = [
+        'lock_thread',
+        'remove_comment',
+        'ban_email',
+        'ban_ip'
+    ]
+
+    def lock_thread(self, request, queryset: QuerySet[Comment]):
+        comments = []
+        stack = list(queryset)
+        while len(stack) > 0:
+            comment = stack.pop()
+            comment.locked = True
+            comments.append(comment)
+
+            for c in comment.children:
+                stack.append(c)
+
+        Comment.objects.bulk_update(comments)
+    lock_thread.short_description = "Lock thread"
+
+    def remove_comment(self, request, queryset: QuerySet[Comment]):
+        queryset.update(published=False)
+    remove_comment.short_description = "Remove comment (without deleting)"
+
+    def ban_email(self, request, queryset: QuerySet[Comment]):
+        BannedEmail.objects.bulk_create([BannedEmail(email=comment.author_email) for comment in queryset])
+    ban_email.short_description = "Ban author email"
+
+    def ban_ip(self, request, queryset: QuerySet[Comment]):
+        BannedIP.objects.bulk_create([BannedIP(ip_addr=comment.ip_addr) for comment in queryset])
+    ban_ip.short_description = "Ban author IP"
+
+
+admin.site.register(Comment)
+admin.site.register(Report)
+admin.site.register(BannedEmail)
+admin.site.register(BannedIP)
+
--- astrid_tech_api/comments/models.py
+++ astrid_tech_api/comments/models.py
@@ -1,7 +1,7 @@
 import bleach
 from django.core.validators import MinLengthValidator
 from django.db.models import Model, EmailField, URLField, CharField, ForeignKey, SET_NULL, CASCADE, BooleanField, \
-    GenericIPAddressField, TextField
+    GenericIPAddressField, TextField, DateField
 from markdown import Markdown
 
 markdown = Markdown()
@@ -29,8 +29,14 @@ class BannedEmail(Model):
 class Comment(Model):
     post = CharField(max_length=100, null=False, db_index=True)
     ip_addr = GenericIPAddressField(null=False)
-    reply_parent = ForeignKey('Comment', null=True, on_delete=SET_NULL, validators=[validate_max_parents])
+    reply_parent = ForeignKey('Comment', null=True, on_delete=CASCADE, validators=[validate_max_parents],
+                              related_name='children')
+    notify_author = BooleanField(default=False)
+
+    authored_date = DateField(auto_now_add=True)
+
     published = BooleanField(default=True)
+    locked = BooleanField(default=False)
 
     author_website = URLField(null=True)
     author_email = EmailField(null=False)
@@ -39,9 +45,19 @@ class Comment(Model):
     content_md = CharField(max_length=1000, null=False, validators=[MinLengthValidator(10)])
     content_html = TextField(null=False, default='')
 
+    @property
+    def can_reply_to(self):
+        return not self.locked and self.published
+
     def save(self, **kwargs):
-        self.content_html = bleach.clean(markdown.convert(self.content_md))
+        if self.published:
+            self.content_html = bleach.clean(markdown.convert(self.content_md))
+        else:
+            self.content_html = '<p>[removed]</p>'
         super().save()
+
+    def __str__(self):
+        return f'Comment #{self.id} by {self.author_name} <{self.author_email}> (re:{self.post} on {self.authored_date})'
 
 
 class Report(Model):
