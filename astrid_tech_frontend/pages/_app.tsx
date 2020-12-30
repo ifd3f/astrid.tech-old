a1795a13732b84bc2d6dfde94cb1fec53bf65707
Astrid Yu <astrid@astrid.tech>
2020-12-29 20:13:45 -0800

    Fix CSS, mostly

--- astrid_tech_frontend/pages/_app.tsx
+++ astrid_tech_frontend/pages/_app.tsx
@@ -1,8 +1,11 @@
+import "@fontsource/gothic-a1";
+import "firacode";
 import { AppProps } from "next/app";
 import React from "react";
 import { TagTableProvider } from "../components/tags/TagTableProvider";
 //require(`katex/dist/katex.min.css`);
 import tags from "../data/tags";
+import "../styles/custom.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
