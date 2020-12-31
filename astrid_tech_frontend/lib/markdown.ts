26c4af85a7056ebe39f576cadd38dad3b02e5bd4
Astrid Yu <astrid@astrid.tech>
2020-12-30 16:40:13 -0800

    Add extensions to Markdown preprocessing

--- astrid_tech_frontend/lib/markdown.ts
+++ astrid_tech_frontend/lib/markdown.ts
@@ -1,18 +1,41 @@
 const smartypants = require("@silvenon/remark-smartypants");
-const remark = require("remark");
+import { join } from "path";
 const graphviz = require("remark-graphviz");
-const html = require("remark-html");
 const math = require("remark-math");
 const prism = require("remark-prism");
+const unwrapImages = require("remark-unwrap-images");
+const picture = require("rehype-picture");
+const remark2rehype = require("remark-rehype");
+const html = require("rehype-stringify");
+const katex = require("rehype-katex");
+const footnotes = require("remark-footnotes");
+const oembed = require("remark-oembed");
+const markdown = require("remark-parse");
+const unified = require("unified");
+const slug = require("remark-slug");
+const urls = require("rehype-urls");
 
-export async function renderMarkdown(md: string) {
-  return (
-    await remark()
-      .use(prism)
-      .use(math)
-      .use(smartypants)
-      .use(html)
-      .use(graphviz)
-      .process(md)
-  ).toString() as string;
+export async function renderMarkdown(md: string, assetRoot: string) {
+  function convertRelative(url: URL) {
+    if (url.hostname != null) return url;
+    return join(assetRoot, url.pathname);
+  }
+
+  const out = await unified()
+    .use(markdown)
+    .use(slug)
+    .use(oembed)
+    .use(graphviz)
+    .use(unwrapImages)
+    .use(smartypants)
+    .use(math)
+    .use(prism)
+    .use(footnotes)
+    .use(remark2rehype)
+    .use(urls, convertRelative)
+    .use(katex)
+    .use(picture)
+    .use(html)
+    .process(md);
+  return out.contents as string;
 }
--- astrid_tech_frontend/pages/[year]/[month]/[day]/[...slug].tsx
+++ astrid_tech_frontend/pages/[year]/[month]/[day]/[...slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import { join } from "path";
 import { FC } from "react";
 import BlogPostPage from "../../../../components/blog/blog";
 import { getBlogPost, getBlogPostSlugs, Path } from "../../../../lib/cache";
@@ -22,7 +23,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const post = getBlogPost(params!! as Path);
 
-  const content = await renderMarkdown(post.content);
+  const content = await renderMarkdown(
+    post.content,
+    join("/_", post.assetRoot)
+  );
 
   `select slug, tag from blog_post
   left join blog_tag
