9362d3de577824442173c828897315adb0b8ee08
Astrid Yu <astrid@astrid.tech>
2021-01-01 15:23:03 -0800

    Skills tuning

--- astrid_tech_frontend/components/resume/Skills.tsx
+++ astrid_tech_frontend/components/resume/Skills.tsx
@@ -1,7 +1,6 @@
 import React, { FC, ReactNode } from "react";
 import { BsStar, BsStarFill } from "react-icons/bs";
-import Masonry from "react-masonry-component";
-import { Col } from "reactstrap";
+import { Col, Row } from "reactstrap";
 import { useTagTable } from "../tags/TagTableProvider";
 import { ALink } from "../util/boilerplate";
 import styleSkills from "./skills.module.scss";
@@ -59,12 +58,21 @@ type SkillCategoryViewProps = {
 const SkillCategoryView: FC<SkillCategoryViewProps> = ({
   category: { name, skills },
 }) => (
-  <Col xs="12" sm="6" lg="4">
-    <h3>{name}</h3>
+  <>
+    <div
+      style={{
+        height: 80,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "end",
+      }}
+    >
+      <h3>{name}</h3>
+    </div>
     {skills.map(({ slug }) => (
       <SkillInfoDisplay slug={slug} key={slug} />
     ))}
-  </Col>
+  </>
 );
 
 export function SkillsSection() {
@@ -73,13 +81,18 @@ export function SkillsSection() {
     <HomepageSection style={{ backgroundColor: "#55cdfc" }}>
       <div className={style.sectionHeading}>
         <h2>Skills</h2>
-        <p>Click on a tag to see related projects and blog posts!</p>
+        <p>
+          Click on <strong>bolded</strong> tags to see related projects and blog
+          posts!
+        </p>
       </div>
-      <Masonry options={{ transitionDuration: 0 }}>
+      <Row options={{ transitionDuration: 0 }}>
         {skills.map((group) => (
-          <SkillCategoryView key={group.name} category={group} />
+          <Col xs="6" sm="4" lg="3">
+            <SkillCategoryView key={group.name} category={group} />
+          </Col>
         ))}
-      </Masonry>
+      </Row>
     </HomepageSection>
   );
 }
