687a17483a1d716da0b1f31c8b56aaa04da2e85f
Astrid Yu <astrid@astrid.tech>
2021-01-11 08:54:19 -0800

    Change MachineTag signature

--- astrid_tech_frontend/lib/MachineTag.test.ts
+++ astrid_tech_frontend/lib/MachineTag.test.ts
@@ -2,18 +2,16 @@ import assert from "assert";
 import { MachineTag } from "./MachineTag";
 
 describe("MachineTag", function () {
-  describe("set tag", function () {
+  describe("constructor", function () {
     it("should parse NPV", function () {
-      const tag = new MachineTag();
-      tag.tag = "foo:bar=spam";
+      const tag = new MachineTag("foo:bar=spam");
       assert.strictEqual(tag.namespace, "foo");
       assert.strictEqual(tag.predicate, "bar");
       assert.strictEqual(tag.value, "spam");
     });
 
     it("should parse NP", function () {
-      const tag = new MachineTag();
-      tag.tag = "foo:bar";
+      const tag = new MachineTag("foo:bar");
       assert.strictEqual(tag.namespace, "foo");
       assert.strictEqual(tag.predicate, "bar");
       assert.strictEqual(tag.value, null);
--- astrid_tech_frontend/lib/MachineTag.ts
+++ astrid_tech_frontend/lib/MachineTag.ts
@@ -1,7 +1,20 @@
+const REGEX = /([a-z0-9-]+):([a-z0-9-]+)(?:=([a-z0-9-]+))?/i;
+
 export class MachineTag {
   public namespace: string = "";
   public predicate: string = "";
   public value: string | null = null;
+
+  constructor(tag: string);
+  constructor(namespace: string, predicate?: string, value?: string) {
+    if (predicate) {
+      this.namespace = namespace;
+      this.predicate = predicate;
+      this.value = value ?? null;
+    } else {
+      this.tag = namespace;
+    }
+  }
 
   get tag() {
     if (this.value) {
@@ -11,12 +24,16 @@ export class MachineTag {
   }
 
   set tag(tag: string) {
-    const match = tag.match(/([a-z0-9-]+):([a-z0-9-]+)(?:=([a-z0-9-]+))?/i);
+    const match = isMachineTag(tag);
     if (match) {
       [, this.namespace, this.predicate, this.value] = match;
-      this.value = this.value ? this.value : null;
+      this.value = this.value ?? null;
     } else {
       throw new Error(`Invalid machine tag ${tag}`);
     }
   }
+}
+
+export function isMachineTag(tag: string) {
+  return tag.match(tag);
 }
