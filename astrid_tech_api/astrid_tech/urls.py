fd912034344060efa198dc1b17e89dd2cb548ac2
Astrid Yu <astrid@astrid.tech>
2021-01-31 21:41:16 -0800

    Fix trailing slash thing

--- astrid_tech_api/astrid_tech/urls.py
+++ astrid_tech_api/astrid_tech/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from comments.views import CommentViewSet
 from printer3d.views import PrinterViewSet
 
-router = DefaultRouter(trailing_slash=False)
+router = DefaultRouter(trailing_slash=True)
 router.register(r'comments', CommentViewSet, basename='comments')
 router.register(r'3dprinter', PrinterViewSet, basename='3dprinter')
 
