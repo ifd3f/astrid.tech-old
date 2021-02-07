1dc8d73769f3eebeda91c157d020a05493b554d4
Astrid Yu <astrid@astrid.tech>
2021-02-07 03:24:25 -0800

    fuck a homie named image optimization

--- astrid_tech_frontend/components/content/ContentImage.tsx
+++ astrid_tech_frontend/components/content/ContentImage.tsx
@@ -2,22 +2,9 @@ import path from "path";
 import React, { ComponentProps, FC } from "react";
 
 export const ContentImage: FC<ComponentProps<"img">> = (props) => {
-  const { src, alt } = props;
-  let url: URL;
-  try {
-    url = new URL(src!);
-  } catch (e) {
-    const ext = path.extname(src!);
-    if (![".jpg", ".jpeg", ".png", ".webp", ".gif"].includes(ext))
-      return <img {...props} />;
-
-    const imgPath = path.join("../../public", src!)
-    const srcSet = require(imgPath + "?resize&sizes[]=300&sizes[]=500&sizes[]=800");
-    return <img {...props} srcSet={srcSet.srcSet} src={srcSet.src} />;
-  }
   return (
-    <a href={src}>
-      <img src={src!} alt={alt} width={1200} />
+    <a href={props.src}>
+      <img {...props} width={1200} />
     </a>
   );
 };
