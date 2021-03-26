6924a4bcb55da3a12f0a222b1b099e0362e86755
Astrid Yu <astrid@astrid.tech>
2021-03-26 16:45:53 -0700

    Use SXG in parser

--- aay_tw_shortener/src/mapper.rs
+++ aay_tw_shortener/src/mapper.rs
@@ -1,9 +1,15 @@
-use crate::mapper::ShortCode::{Blog, Project};
-use crate::mapper::ShortCodeParseError::{EmptyString, TooLong};
+use std::ops::Add;
+
+use chrono::{Datelike, Duration, FixedOffset, NaiveDate, NaiveDateTime};
+
+use crate::mapper::ShortCode::{Blog, Project, Text};
+use crate::mapper::ShortCodeParseError::{EmptyString, SXGError, TooLong, UnsupportedType};
+use crate::newbase60::sxg_to_num;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum ShortCode<'a> {
-    Blog(u32, u8, u8, Option<u8>),
+    Blog(i32, u32, u32),
+    Text(i32, u32, u32, u32),
     Project(&'a str),
 }
 
@@ -11,8 +17,13 @@ enum ShortCode<'a> {
 pub enum ShortCodeParseError {
     EmptyString,
     InvalidDate,
-    InvalidChar,
+    SXGError,
     TooLong,
+    UnsupportedType,
+}
+
+fn days_after_epoch(n: i64) -> NaiveDate {
+    NaiveDate::from_yo(1970, 1).add(Duration::days(n))
 }
 
 impl ShortCode<'_> {
@@ -21,24 +32,47 @@ impl ShortCode<'_> {
             return Err(ShortCodeParseError::TooLong);
         }
 
-        match shortcode.chars().next() {
-            Some('b') => Ok(Blog(0, 0, 0, Some(0))),
-            Some('p') => Ok(Project(&shortcode[1..])),
-            _ => Err(ShortCodeParseError::EmptyString),
+        let code_type = match shortcode.chars().next() {
+            Some(c) => c,
+            None => return Err(ShortCodeParseError::EmptyString),
+        };
+
+        match code_type {
+            'b' => {
+                let number = match sxg_to_num(&shortcode[1..]) {
+                    Some(n) => n,
+                    None => return Err(SXGError),
+                };
+
+                let date = days_after_epoch(number as i64);
+                Ok(Blog(date.year(), date.month(), date.day()))
+            }
+            't' => {
+                let number = match sxg_to_num(&shortcode[1..]) {
+                    Some(n) => n,
+                    None => return Err(SXGError),
+                };
+
+                let epoch_days = (number / 60) as i64;
+                let ordinal = (number % 60) as u32;
+                let date = days_after_epoch(epoch_days);
+                Ok(Text(date.year(), date.month(), date.day(), ordinal))
+            }
+            'p' => Ok(Project(&shortcode[1..])),
+            _ => Err(ShortCodeParseError::UnsupportedType),
         }
     }
 
     fn expand(&self) -> String {
         match self {
-            Blog(y, m, d, n) => {
-                if let Some(n) = n {
-                    format!("{}/{:0>2}/{:0>2}/{}", y, m, d, n)
-                } else {
-                    format!("{}/{:0>2}/{:0>2}", y, m, d)
-                }
+            Blog(y, m, d) => {
+                format!("{}/{:0>2}/{:0>2}/", y, m, d)
+            }
+            Text(y, m, d, n) => {
+                format!("{}/{:0>2}/{:0>2}/{}/", y, m, d, n)
             }
             Project(p) => {
-                format!("projects/{}", p)
+                format!("projects/{}/", p)
             }
         }
     }
@@ -50,21 +84,33 @@ pub fn expand_shortcode(shortcode: &str) -> Result<String, ShortCodeParseError> 
 }
 
 #[test]
-fn expands_blog_without_ord() {
-    let code = Blog(2021, 3, 28, None);
-    assert_eq!(code.expand(), "2021/03/28".to_string())
+fn parses_blog() {
+    let code = ShortCode::parse("b4MY").unwrap();
+    assert_eq!(code, Blog(2012, 12, 18))
 }
 
 #[test]
-fn expands_blog_with_ord() {
-    let code = Blog(2021, 3, 28, Some(9));
-    assert_eq!(code.expand(), "2021/03/28/9".to_string())
+fn parses_text() {
+    let code = ShortCode::parse("t4MYA").unwrap();
+    assert_eq!(code, Text(2012, 12, 18, 10))
+}
+
+#[test]
+fn expands_blog() {
+    let code = Blog(2021, 3, 28);
+    assert_eq!(code.expand(), "2021/03/28/".to_string())
+}
+
+#[test]
+fn expands_text() {
+    let code = Text(2021, 3, 28, 9);
+    assert_eq!(code.expand(), "2021/03/28/9/".to_string())
 }
 
 #[test]
 fn expands_project() {
     let code = Project("asdf");
-    assert_eq!(code.expand(), "projects/asdf".to_string())
+    assert_eq!(code.expand(), "projects/asdf/".to_string())
 }
 
 #[test]
@@ -75,4 +121,9 @@ fn does_not_parse_empty_codes() {
 #[test]
 fn does_not_parse_long_codes() {
     assert_eq!(ShortCode::parse("sfd8977f879978sdf}"), Err(TooLong))
+}
+
+#[test]
+fn does_not_parse_unsupported_types() {
+    assert_eq!(ShortCode::parse("f32"), Err(UnsupportedType))
 }
