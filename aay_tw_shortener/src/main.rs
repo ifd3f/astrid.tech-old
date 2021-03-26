eba215c8b99e906e8f1e688c0efb10e1803e2c5a
Astrid Yu <astrid@astrid.tech>
2021-03-26 13:53:33 -0700

    Write some tests + expansion code

--- aay_tw_shortener/src/main.rs
+++ aay_tw_shortener/src/main.rs
@@ -1,9 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+#![feature(in_band_lifetimes)]
 
+extern crate dotenv;
 #[macro_use]
 extern crate rocket;
-use rocket::http::RawStr;
+
+use std::env;
+
+use dotenv::dotenv;
+use rocket::http::{RawStr, Status};
 use rocket::response::Redirect;
+
+use mapper::expand_shortcode;
+use rocket::response::status::NotFound;
+use rocket::Response;
+
+mod mapper;
 
 #[get("/")]
 fn index() -> Redirect {
@@ -11,11 +23,20 @@ fn index() -> Redirect {
 }
 
 #[get("/<code>")]
-fn lengthen(code: &RawStr) -> Redirect {
-    "Hello, world!"
+fn lengthen(code: &RawStr) -> Result<Redirect, Status> {
+    let expanded = match expand_shortcode(code) {
+        Ok(path) => path,
+        Err(_) => return Err(Status::NotFound),
+    };
+
+    let uri = format!("{}/{}", "http://astrid.tech", expanded);
+
+    Ok(Redirect::to(uri))
 }
 
 fn main() {
+    dotenv().ok();
+
     rocket::ignite()
         .mount("/", routes![index, lengthen])
         .launch();
--- aay_tw_shortener/src/mapper.rs
+++ aay_tw_shortener/src/mapper.rs
@@ -1,13 +1,78 @@
+use crate::mapper::ShortCode::{Blog, Project};
+use crate::mapper::ShortCodeParseError::{EmptyString, TooLong};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum ShortCode<'a> {
-    Blog(u32),
+    Blog(u32, u8, u8, Option<u8>),
     Project(&'a str),
 }
 
-fn parse_shortcode(shortcode: &'a str) -> ShortCode<'a> {
-    match shortcode[0] {
-        'b' => Blog(0),
-        'p' => Project(shortcode[1..]),
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ShortCodeParseError {
+    EmptyString,
+    InvalidDate,
+    InvalidChar,
+    TooLong,
+}
+
+impl ShortCode<'_> {
+    fn parse(shortcode: &'a str) -> Result<ShortCode<'a>, ShortCodeParseError> {
+        if shortcode.len() > 12 {
+            return Err(ShortCodeParseError::TooLong);
+        }
+
+        match shortcode.chars().next() {
+            Some('b') => Ok(Blog(0, 0, 0, Some(0))),
+            Some('p') => Ok(Project(&shortcode[1..])),
+            _ => Err(ShortCodeParseError::EmptyString),
+        }
+    }
+
+    fn expand(&self) -> String {
+        match self {
+            Blog(y, m, d, n) => {
+                if let Some(n) = n {
+                    format!("{}/{:0>2}/{:0>2}/{}", y, m, d, n)
+                } else {
+                    format!("{}/{:0>2}/{:0>2}", y, m, d)
+                }
+            }
+            Project(p) => {
+                format!("projects/{}", p)
+            }
+        }
     }
 }
 
-fn translate_shortcode(shortcode: &str) -> String {}
+pub fn expand_shortcode(shortcode: &str) -> Result<String, ShortCodeParseError> {
+    let parsed = ShortCode::parse(shortcode)?;
+    Ok(parsed.expand())
+}
+
+#[test]
+fn expands_blog_without_ord() {
+    let code = Blog(2021, 3, 28, None);
+    assert_eq!(code.expand(), "2021/03/28".to_string())
+}
+
+#[test]
+fn expands_blog_with_ord() {
+    let code = Blog(2021, 3, 28, Some(9));
+    assert_eq!(code.expand(), "2021/03/28/9".to_string())
+}
+
+#[test]
+fn expands_project() {
+    let code = Project("asdf");
+    assert_eq!(code.expand(), "projects/asdf".to_string())
+}
+
+#[test]
+fn does_not_parse_empty_codes() {
+    assert_eq!(ShortCode::parse(""), Err(EmptyString))
+}
+
+#[test]
+fn does_not_parse_long_codes() {
+    assert_eq!(ShortCode::parse("sfd8977f879978sdf}"), Err(TooLong))
+}
