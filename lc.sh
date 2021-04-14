d6660ef60ac0bb68f9bf70a5a56df6b6cc2f1aa9
Astrid Yu <astrid@astrid.tech>
2021-04-13 17:58:48 -0700

    Remove content directory

--- lc.sh
+++ lc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-find . -type f \( -name '*.tsx' -or -name '*.ts' -or -name '*.jsx' -or -name '*.js' -or -name '*.scss' -or -name '*.py' \) \
+path=$1
+
+find ${path-.} -type f \( -name '*.tsx' -or -name '*.ts' -or -name '*.jsx' -or -name '*.js' -or -name '*.scss' -or -name '*.py' \) \
   -not -path "./astrid_tech_frontend/node_modules/*" \
   -not -path "./astrid_tech_frontend/.next/*" \
   -not -path "./astrid_tech_frontend/out/*" \
