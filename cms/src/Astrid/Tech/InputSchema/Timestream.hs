bf04fd297fc27460293869a8b642424b387e4b5f
Astrid Yu <astrid@astrid.tech>
2021-04-26 01:12:10 -0700

    man i am trash at fp

--- cms/src/Astrid/Tech/InputSchema/Timestream.hs
+++ cms/src/Astrid/Tech/InputSchema/Timestream.hs
@@ -1,8 +1,10 @@
 module Astrid.Tech.InputSchema.Timestream (Timestream (..)) where
 
+import Data.Either (partitionEithers)
+import Data.Either.Combinators (mapRight)
 import qualified Data.Map as Map
 import Data.Maybe (mapMaybe, maybeToList)
-import System.Directory.Tree (DirTree (Dir), contents)
+import System.Directory.Tree (AnchoredDirTree ((:/)), DirTree (Dir))
 import qualified System.Directory.Tree as DT
 import System.FilePath ((</>))
 import Text.Read (readMaybe)
@@ -12,36 +14,62 @@ data Timestream e d m y = Timestream
   }
 
 data Year e d m y = Year
-  { yearObjects :: [y],
-    months :: Map.Map Integer (Month e d m)
+  { months :: Map.Map Integer (Month e d m),
+    yearObjects :: [y]
   }
 
 data Month e d m = Month
-  { monthObjects :: [m],
-    days :: Map.Map Int (Day e d)
+  { days :: Map.Map Int (Day e d),
+    monthObjects :: [m]
   }
 
 data Day e d = Day
-  { dayObjects :: [d],
-    entries :: Map.Map Int e
+  { entries :: Map.Map Int e,
+    dayObjects :: [d]
   }
 
-maybeNumericDirectory (Dir name ords) = do
-  ord <- readMaybe name :: Maybe a
-  pure (ord, Dir name ords)
-maybeNumericDirectory invalid = Nothing
+liftMultiError f anchor subDirs = case partitionEithers subDirs of
+  ([], successes) -> Right $ f successes
+  (errors, _) -> Left $ map (anchor </>) (concat errors)
 
-dirToDay ((DT.:/) anchor dir) = case dir of
+-- | A dreadful horror show of a function that abstracts away several things.
+-- |
+-- | First argument constructs the parent object from its child directories and its list of index children if it has any.
+-- | Second argument constructs the child objects, or errors with a list of failing files.
+-- | Third argument is the root of the directory to read.
+readNumericDir ::
+  (Ord k, Read k) =>
+  (Map.Map k child -> [AnchoredDirTree dt] -> r) ->
+  (AnchoredDirTree dt -> Either [FilePath] child) ->
+  AnchoredDirTree dt ->
+  Either [FilePath] r
+readNumericDir constructParent fChildren (anchor :/ dir) = case dir of
   Dir _ children ->
-    let ordinalDirs = mapMaybe maybeNumericDirectory children
-        index = do
-          indexDir <- maybeToList $ DT.dropTo "index" ((DT.:/) anchor dir)
+    let subDirs =
+          mapMaybe
+            ( \childDir -> do
+                let dirName = DT.name childDir
+                key <- readMaybe dirName
+                let anchoredChildDir = (anchor </> dirName) :/ childDir
+                pure $ mapRight (\childObj -> (key, childObj)) $ fChildren anchoredChildDir
+            )
+            children
+
+        indexObjects = do
+          indexDir <- maybeToList $ DT.dropTo "index" (anchor :/ dir)
           case DT.dirTree indexDir of
-            Dir _ indexContents -> indexContents
+            Dir dirName indexContents -> map ((anchor </> dirName) :/) indexContents
             _ -> []
-     in Right $
-          Day
-            { dayObjects = index,
-              entries = Map.fromList ordinalDirs
-            }
-  invalid -> Left $ anchor </> DT.name invalid
+     in case partitionEithers subDirs of
+          ([], successes) -> Right $ constructParent (Map.fromList successes) indexObjects
+          (errors, _) -> Left $ map (anchor </>) (concat errors)
+  invalid -> Left $ [anchor </> DT.name invalid]
+
+readDay :: AnchoredDirTree a -> Either [FilePath] (Day (AnchoredDirTree a) (AnchoredDirTree a))
+readDay = readNumericDir Day Right
+
+readMonth :: AnchoredDirTree a -> Either [FilePath] (Month (AnchoredDirTree a) (AnchoredDirTree a) (AnchoredDirTree a))
+readMonth = readNumericDir Month readDay
+
+readYear :: AnchoredDirTree a -> Either [FilePath] (Year (AnchoredDirTree a) (AnchoredDirTree a) (AnchoredDirTree a) (AnchoredDirTree a))
+readYear = readNumericDir Year readMonth
