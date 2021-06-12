781b3ee57dcbbacc78b093ccefe39bbe0943e2ee
Astrid Yu <astrid@astrid.tech>
2021-06-12 11:37:56 -0700

    Content types

--- astrid_tech_cms/src/content/post.rs
+++ astrid_tech_cms/src/content/post.rs
@@ -1,4 +1,6 @@
+use std::borrow::Borrow;
 use std::convert::TryFrom;
+use std::str::FromStr;
 
 use chrono::{DateTime, Utc};
 use gray_matter::engine::yaml::YAML;
@@ -6,12 +8,39 @@ use gray_matter::matter::Matter;
 use gray_matter::value::pod::Pod;
 use serde::{Deserialize, Serialize};
 use vfs::{VfsFileType, VfsPath};
-use std::borrow::Borrow;
 
+#[derive(Eq, PartialEq, Debug)]
+pub enum ContentType {
+    Markdown,
+    JupyterNotebook,
+    Text,
+}
+
+#[derive(Eq, PartialEq, Debug)]
+struct UnsupportedContentType(String);
+
+impl ContentType {
+    fn from_ext(ext: &str) -> Result<Self, UnsupportedContentType> {
+        match ext {
+            "md" | "markdown" => Ok(ContentType::Markdown),
+            "ipynb" => Ok(ContentType::JupyterNotebook),
+            "txt" => Ok(ContentType::Text),
+            _ => Err(UnsupportedContentType(ext.to_string()))
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct PostContent {
+    content_type: ContentType,
+    content: String,
+}
+
+#[derive(Eq, PartialEq, Debug)]
 pub struct Post {
     name: String,
-    file_contents: String,
-    meta: EmbeddedMeta
+    content: PostContent,
+    meta: EmbeddedMeta,
 }
 
 impl Post {
@@ -26,6 +55,7 @@ pub enum PostError {
     YAML(serde_yaml::Error),
     Serde(serde_json::error::Error),
     AmbiguousIndex(FindIndexError),
+    UnsupportedContentType(UnsupportedContentType),
 }
 
 impl From<vfs::VfsError> for PostError {
@@ -46,19 +76,25 @@ impl From<serde_json::Error> for PostError {
     }
 }
 
+impl From<UnsupportedContentType> for PostError {
+    fn from(e: UnsupportedContentType) -> Self {
+        PostError::UnsupportedContentType(e)
+    }
+}
+
 impl From<FindIndexError> for PostError {
     fn from(e: FindIndexError) -> Self {
         PostError::AmbiguousIndex(e)
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 struct ImageEntry {
     image: String,
     caption: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 #[serde(tag = "type", rename_all = "camelCase")]
 enum PostType {
     #[serde(rename = "article")]
@@ -81,7 +117,7 @@ enum PostType {
     },
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 #[serde(rename_all = "camelCase")]
 struct EmbeddedMeta {
     date: DateTime<Utc>,
@@ -126,7 +162,7 @@ impl TryFrom<&VfsPath> for Post {
     type Error = PostError;
 
     fn try_from(path: &VfsPath) -> Result<Self, Self::Error> {
-        let (name, meta, file_contents) = match path.metadata()?.file_type {
+        let (name, meta, content) = match path.metadata()?.file_type {
             VfsFileType::File => {
                 todo!()
             }
@@ -140,7 +176,8 @@ impl TryFrom<&VfsPath> for Post {
                         let file = index_file.open_file()?;
                         let meta: SeparateYAMLMeta = serde_yaml::from_reader(file)?;
                         let content_file = path.join(meta.content_path.as_str())?;
-                        (meta.meta, content_file.read_to_string()?)
+                        let content_type = ContentType::from_ext(content_file.extension().unwrap().as_str())?;
+                        (meta.meta, PostContent { content: content_file.read_to_string()?, content_type })
                     }
                     "index.md" => {
                         let contents = {
@@ -151,7 +188,7 @@ impl TryFrom<&VfsPath> for Post {
                         let matter = Matter::<YAML>::new();
                         let parsed = matter.matter(contents);
                         let meta: EmbeddedMeta = parsed.data.deserialize()?;
-                        (meta, parsed.content)
+                        (meta, PostContent { content: parsed.content, content_type: ContentType::Markdown })
                     }
                     _ => {
                         todo!()
@@ -164,17 +201,19 @@ impl TryFrom<&VfsPath> for Post {
 
         Ok(Post {
             name,
-            file_contents,
-            meta
+            content,
+            meta,
         })
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::content::post::{Post, EmbeddedMeta, PostType, SeparateYAMLMeta};
-    use vfs::{MemoryFS, VfsPath};
     use std::convert::TryFrom;
+
+    use vfs::{MemoryFS, VfsPath};
+
+    use crate::content::post::{ContentType, EmbeddedMeta, Post, PostType, SeparateYAMLMeta};
 
     const TXT_ARTICLE_YAML: &str = r#"
         date: 2021-06-12 10:51:30 +08:00
@@ -207,12 +246,13 @@ mod test {
     fn parses_article_meta() {
         let parsed: SeparateYAMLMeta = serde_yaml::from_str(TXT_ARTICLE_YAML).unwrap();
 
-        if let PostType::Article { title, description } = parsed.meta.post_type {
-            assert_eq!(title, "Example post with txt");
-            assert_eq!(description, None);
-        } else {
-            panic!("Post is not an Article")
-        }
+        assert_eq!(
+            parsed.meta.post_type,
+            PostType::Article {
+                title: "Example post with txt".to_string(),
+                description: None,
+            }
+        );
     }
 
     #[test]
@@ -221,6 +261,7 @@ mod test {
 
         let post = Post::try_from(&path).unwrap();
 
-        assert_eq!(post.file_contents, TXT_CONTENTS)
+        assert_eq!(post.content.content_type, ContentType::Text);
+        assert_eq!(post.content.content, TXT_CONTENTS);
     }
 }
