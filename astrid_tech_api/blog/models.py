4ca160df516e7d56414154249d35e7ec200f62dd
Astrid Yu <astrid@astrid.tech>
2021-06-17 12:51:58 -0700

    Define models for the blog

--- astrid_tech_api/blog/models.py
+++ astrid_tech_api/blog/models.py
@@ -1,3 +1,66 @@
-from django.db import models
+from django.db.models import Model, TextField, CharField, UUIDField, IntegerField, DateTimeField, URLField, \
+    ManyToManyField, ForeignKey, CASCADE
 
-# Create your models here.
+
+class Tag(Model):
+    short_name = CharField(max_length=32, null=False, blank=False, primary_key=True)
+    name = CharField(max_length=32)
+    color = CharField(max_length=10)
+    background_color = CharField(max_length=10)
+    description = TextField(null=True, blank=True)
+
+
+class Entry(Model):
+    uuid = UUIDField(primary_key=True)
+
+    name = TextField()
+    summary = TextField()
+    short_name = TextField()
+
+    created_date = DateTimeField()
+    """When this entry was originally created. Unless """
+    published_date = DateTimeField()
+    """When this entry was, or will be, published."""
+    updated_date = DateTimeField(auto_now=True)
+    """When this entry was last updated."""
+    ordinal = IntegerField(null=False)
+    """The ordinal entry for the day."""
+
+    reply_to = URLField()
+    """What this is in reply to."""
+    location = URLField()
+    repost_of = URLField()
+    tags = ManyToManyField(Tag)
+
+    content_type = TextField()
+    """The content type, as a mimetype."""
+    content = TextField()
+    """The content of this entry."""
+
+
+class Attachment(Model):
+    entry = ForeignKey(Entry, on_delete=CASCADE)
+    url = URLField()
+    caption = TextField()
+
+
+class Syndications(Model):
+    entry = ForeignKey(Entry, on_delete=CASCADE)
+    target = URLField()
+
+
+class Project(Model):
+    uuid = UUIDField(primary_key=True)
+
+    title = TextField()
+    description = TextField()
+    short_name = TextField()
+
+    start_date = DateTimeField()
+    end_date = DateTimeField()
+    published_date = DateTimeField()
+    updated_date = DateTimeField()
+
+    tags = ManyToManyField(Tag)
+
+    content = TextField()
---
+++ astrid_tech_api/blog/serializer.py
@@ -0,0 +1,20 @@
+from django.forms import DateTimeField
+from rest_framework.serializers import Serializer, CharField, URLField
+
+
+class MicropubEntrySerializer(Serializer):
+    h = CharField()
+
+    name = CharField()
+    summary = CharField()
+    content = CharField()
+
+    published = DateTimeField()
+    updated = DateTimeField()
+    category = CharField(many=True)
+
+    in_reply_to = URLField(source='in-reply-to')
+    repost_of = URLField(source='repost-of')
+
+    syndication = URLField(many=True)
+    mp_syndicate_to = URLField(source='mp-syndicate-to', many=True)
