250ab83ea6d012b9176d3d4ddb18dfc394a9a389
Astrid Yu <astrid@astrid.tech>
2021-06-20 11:38:01 -0700

    Write sad paths for indieauth

--- astrid_tech_api/indieauth/tests/test_indieauth.py
+++ astrid_tech_api/indieauth/tests/test_indieauth.py
@@ -8,7 +8,7 @@ from freezegun import freeze_time
 from indieauth.models import ConsentRequest
 from indieauth.util import get_uri_params
 
-working_params = dict(
+working_id_params = dict(
     me='https://astrid.tech/',
     client_id='https://webapp.example.org/',
     redirect_uri='https://webapp.example.org/auth/callback?some=param',
@@ -32,23 +32,51 @@ class TestIndieAuthFlow(TestCase):
     def login_allowed(self):
         self.client.force_login(self.allowed_user)
 
+    def authorize_with_code(self, code, accept='application/json'):
+        response = self.client.post(
+            '/auth/indieauth',
+            {
+                'code': code,
+                'redirect_uri': working_id_params['redirect_uri'],
+                'client_id': working_id_params['client_id']
+            },
+            HTTP_ACCEPT=accept
+        )
+        return response
+
+    @freeze_time(confirm_time)
+    def test_anonymous_get_endpoint_is_redirected(self):
+        self.client.logout()
+
+        response = self.client.get('/auth/indieauth', working_id_params)
+
+        self.assertEqual(302, response.status_code)
+
+    @freeze_time(confirm_time)
+    def setup_get_endpoint(self):
+        self.login_allowed()
+        self.client.get('/auth/indieauth', working_id_params)
+        return ConsentRequest.objects.get()
+
+    @freeze_time(confirm_time)
+    def setup_confirm_consent_then_logout(self):
+        obj = self.setup_get_endpoint()
+        response = self.client.post('/auth/indieauth/confirm', {'request_id': obj.pk})
+        qs = get_uri_params(response.headers['Location'])
+        self.client.logout()
+        return obj, qs
+
     @freeze_time(confirm_time)
     def test_get_endpoint_creates_consent_request(self):
         self.login_allowed()
 
-        response = self.client.get('/auth/indieauth', working_params)
+        response = self.client.get('/auth/indieauth', working_id_params)
 
         self.assertEqual(200, response.status_code)
         obj = ConsentRequest.objects.get()
         self.assertFalse(obj.confirmed)
         self.assertEqual(len(obj.auth_code), 0)
-        self.assertEqual(working_params['redirect_uri'], obj.redirect_uri)
-
-    @freeze_time(confirm_time)
-    def setup_get_endpoint(self):
-        self.login_allowed()
-        self.client.get('/auth/indieauth', working_params)
-        return ConsentRequest.objects.get()
+        self.assertEqual(working_id_params['redirect_uri'], obj.redirect_uri)
 
     @freeze_time(confirm_time)
     def test_post_confirm_confirms_consent_request(self):
@@ -59,32 +87,57 @@ class TestIndieAuthFlow(TestCase):
         obj = ConsentRequest.objects.get(pk=obj.pk)
         qs = get_uri_params(response.headers['Location'])
         self.assertEqual('param', qs['some'])
-        self.assertEqual(working_params['state'], qs['state'])
+        self.assertEqual(working_id_params['state'], qs['state'])
         self.assertEqual(obj.auth_code, qs['code'])
         self.assertEqual(302, response.status_code)
         self.assertTrue(obj.confirmed)
         self.assertGreater(len(obj.auth_code), 0)
 
     @freeze_time(confirm_time)
-    def setup_confirm_consent(self):
+    def test_post_endpoint_authorizes_app(self):
+        obj, qs = self.setup_confirm_consent_then_logout()
+
+        response = self.authorize_with_code(qs['code'])
+
+        self.assertFalse(ConsentRequest.objects.filter(pk=obj.pk).exists())
+        self.assertEqual(200, response.status_code)
+        self.assertEqual({'me': 'https://astrid.tech/'}, response.json())
+
+    @freeze_time(expired_time)
+    def test_expired_consent_fails(self):
         obj = self.setup_get_endpoint()
+
         response = self.client.post('/auth/indieauth/confirm', {'request_id': obj.pk})
-        qs = get_uri_params(response.headers['Location'])
-        return qs
+
+        self.assertEqual(401, response.status_code)
+        self.assertFalse(ConsentRequest.objects.filter(pk=obj.pk).exists())
+
+    @freeze_time(expired_time)
+    def test_expired_authorize_fails(self):
+        obj, qs = self.setup_confirm_consent_then_logout()
+
+        response = self.authorize_with_code(qs['code'])
+
+        self.assertEqual(401, response.status_code)
+        self.assertFalse(ConsentRequest.objects.filter(pk=obj.pk).exists())
 
     @freeze_time(confirm_time)
-    def test_post_endpoint_authorizes_app(self):
-        qs = self.setup_confirm_consent()
-
+    def test_authorize_without_enough_params_fails(self):
         response = self.client.post(
             '/auth/indieauth',
             {
-                'code': qs['code'],
-                'redirect_uri': working_params['redirect_uri'],
-                'client_id': working_params['client_id']
+                'redirect_uri': working_id_params['redirect_uri'],
+                'client_id': working_id_params['client_id']
             },
             HTTP_ACCEPT='application/json'
         )
 
-        self.assertEqual(200, response.status_code)
-        self.assertEqual({'me': 'https://astrid.tech/'}, response.json())
+        self.assertEqual(401, response.status_code)
+
+    @freeze_time(confirm_time)
+    def test_authorize_with_invalid_code_fails(self):
+        obj, qs = self.setup_confirm_consent_then_logout()
+
+        response = self.authorize_with_code("This ain't a valid code!")
+
+        self.assertEqual(401, response.status_code)
--- astrid_tech_api/indieauth/views.py
+++ astrid_tech_api/indieauth/views.py
@@ -7,6 +7,7 @@ from django.db import transaction
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
+from rest_framework.status import *
 from structlog import get_logger
 
 from .models import ConsentRequest
@@ -15,68 +16,79 @@ logger = get_logger(__name__)
 
 
 @login_required
+def _ask_for_consent(request):
+    me = request.GET.get('me')
+    client_id = request.GET.get('client_id')
+    redirect_uri = request.GET.get('redirect_uri')
+    state = request.GET.get('state')
+    response_type = request.GET.get('response_type', 'id')
+    scope = request.GET.get('scope', '')
+
+    logger_ = logger.bind(me=me, client_id=client_id, redirect_uri=redirect_uri, state=state,
+                          response_type=response_type)
+
+    if me not in ['https://astrid.tech', 'https://astrid.tech/']:
+        logger_.warn('Unsupported me param')
+        return HttpResponse(f'cannot authorize {me}', status=HTTP_400_BAD_REQUEST)
+
+    # required fields
+    for field in [client_id, redirect_uri, state]:
+        if field is None:
+            return HttpResponse('missing fields from query', status=HTTP_400_BAD_REQUEST)
+
+    if response_type == 'id':
+        pass
+
+    ConsentRequest.objects.filter(client_id=client_id).delete()
+
+    perm_rq = ConsentRequest.objects.create(
+        client_id=client_id,
+        me=me,
+        state=state,
+        response_type=response_type,
+        redirect_uri=redirect_uri,
+        scope=scope,
+        expires_at=datetime.now(pytz.utc) + timedelta(minutes=5),
+        confirmed=False
+    )
+    logger_.info('Rendering form', perm_rq=perm_rq)
+
+    return render(request, 'indieauth/authorize.html', {
+        'perm_rq': perm_rq
+    })
+
+
+def _authorize_indieauth(request):
+    redirect_uri = request.POST.get('redirect_uri')
+    code = request.POST.get('code')
+    client_id = request.POST.get('client_id')
+    try:
+        obj = ConsentRequest.objects.get(client_id=client_id, redirect_uri=redirect_uri, auth_code=code)
+    except ConsentRequest.DoesNotExist:
+        return HttpResponse('request expired', status=HTTP_401_UNAUTHORIZED)
+
+    if obj.has_expired:
+        obj.delete()
+        return HttpResponse('request expired', status=HTTP_401_UNAUTHORIZED)
+
+    # TODO create authorization
+
+    obj.delete()
+
+    response = {'me': 'https://astrid.tech/'}
+    if request.headers.get('Accept') == 'application/json':
+        return JsonResponse(response, status=HTTP_200_OK)
+    else:
+        return HttpResponse(urlencode(response), status=HTTP_200_OK)
+
+
+@require_http_methods(['GET', 'POST'])
 def auth_consent(request):
     if request.method == 'GET':
-        me = request.GET.get('me')
-        client_id = request.GET.get('client_id')
-        redirect_uri = request.GET.get('redirect_uri')
-        state = request.GET.get('state')
-        response_type = request.GET.get('response_type', 'id')
-        scope = request.GET.get('scope', '')
-
-        logger_ = logger.bind(me=me, client_id=client_id, redirect_uri=redirect_uri, state=state,
-                              response_type=response_type)
-
-        if me not in ['https://astrid.tech', 'https://astrid.tech/']:
-            logger_.warn('Unsupported me param')
-            return HttpResponse(f'cannot authorize {me}', status=400)
-
-        # required fields
-        for field in [client_id, redirect_uri, state]:
-            if field is None:
-                return HttpResponse('missing fields from query', status=400)
-
-        if response_type == 'id':
-            pass
-
-        ConsentRequest.objects.filter(client_id=client_id).delete()
-
-        perm_rq = ConsentRequest.objects.create(
-            client_id=client_id,
-            me=me,
-            state=state,
-            response_type=response_type,
-            redirect_uri=redirect_uri,
-            scope=scope,
-            expires_at=datetime.now(pytz.utc) + timedelta(minutes=5),
-            confirmed=False
-        )
-        logger_.info('Rendering form', perm_rq=perm_rq)
-
-        return render(request, 'indieauth/authorize.html', {
-            'perm_rq': perm_rq
-        })
+        return _ask_for_consent(request)
 
     if request.method == 'POST':
-        redirect_uri = request.POST.get('redirect_uri')
-        code = request.POST.get('code')
-        client_id = request.POST.get('client_id')
-        try:
-            obj = ConsentRequest.objects.get(client_id=client_id, redirect_uri=redirect_uri, auth_code=code)
-        except ConsentRequest.DoesNotExist:
-            return HttpResponse('request expired', status=498)
-
-        if obj.has_expired:
-            obj.delete()
-            return HttpResponse('request expired', status=498)
-
-        # TODO create authorization
-
-        response = {'me': 'https://astrid.tech/'}
-        if request.headers.get('Accept') == 'application/json':
-            return JsonResponse(response, status=200)
-        else:
-            return HttpResponse(urlencode(response), status=200)
+        return _authorize_indieauth(request)
 
 
 @login_required
@@ -89,7 +101,7 @@ def auth_confirm(request):
 
     if obj.expires_at <= datetime.now(pytz.utc):
         obj.delete()
-        return HttpResponse('request expired', status=498)
+        return HttpResponse('request expired', status=HTTP_401_UNAUTHORIZED)
 
     obj.confirm()
     obj.save()
