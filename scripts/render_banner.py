aa61ed934caf3e54eaae4538e801585a0d4976fd
Astrid Yu <astrid@astrid.tech>
2021-09-12 16:28:27 -0700

    Improve the 88x31

--- scripts/render_banner.py
+++ scripts/render_banner.py
@@ -12,6 +12,15 @@ def dark_to_alpha(im):
                 im.putpixel((x, y), TRANSPARENT)
 
 
+def set_alpha(im, a):
+    w, h = im.size
+    for y in range(h):
+        for x in range(w):
+            r, g, b, a_orig = im.getpixel((x, y))
+            if a_orig > 0:
+                im.putpixel((x, y), (r, g, b, a))
+
+
 def shooting_frames():
     orig = Image.open("assets/shootingstar.gif")
     for i in range(0, orig.n_frames - 1):
@@ -20,6 +29,7 @@ def shooting_frames():
         w, h = conv.size
         conv = conv.crop((w * 0.2, 0, w * 0.8, h))
         dark_to_alpha(conv)
+        set_alpha(conv, 80)
         yield conv
 
 
@@ -30,20 +40,36 @@ def twinkling_frames():
         yield im
 
 
-def words_img():
-    # Comes from https://cooltext.com/Logo-Design-Princess
-    im = Image.open("assets/text.png")
-    return im
+def astral_frames():
+    # from https://cooltext.com/Logo-Design-Glitter
+    im = Image.open("assets/astral.gif")
+    for i in range(0, im.n_frames):
+        im.seek(i)
+        conv = im.convert('RGBA')
+        conv = conv.resize((80, 20))
+        dark_to_alpha(conv)
+        yield conv
+
+
+def domain_frames():
+    # Comes from https://cooltext.com/Logo-Design-Blinkie
+    im = Image.open("assets/domain.gif")
+    for i in range(0, im.n_frames):
+        im.seek(i)
+        conv = im.convert('RGBA')
+        conv = conv.resize((80, 30))
+        dark_to_alpha(conv)
+        yield conv
 
 
 def rendered_frames():
-    words = words_img()
-    frames = zip(shooting_frames(), twinkling_frames())
-    for shooting, twinkling in frames:
-        background = twinkling.crop((0, 10, 88, 50)).resize((88, 31))
+    frames = zip(shooting_frames(), twinkling_frames(), astral_frames(), domain_frames())
+    for shooting, twinkling, astral, domain in frames:
+        background = twinkling.crop((0, 10, 100, 50)).resize((88, 31))
         shooting2 = shooting.resize((88, 31))
         background.paste(shooting2, (0, 0), shooting2)
-        background.paste(words, (-10, 0), words)
+        background.paste(astral, (-4, 0), astral)
+        background.paste(domain, (14, 10), domain)
         yield background.convert("RGB")
 
 
