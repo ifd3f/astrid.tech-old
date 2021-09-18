b636397ab5a897d1194661b54643d1d875ddb834
Astrid Yu <astrid@astrid.tech>
2021-09-18 20:22:57 +0000

    Add git user and email to entrypoint

--- wm-receiver/entrypoint.sh
+++ wm-receiver/entrypoint.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env sh
 
+fatal_error {
+    echo $1
+    exit 1
+}
+
+if [ -z $GIT_EMAIL ]; then 
+    fatal_error "Must provide GIT_EMAIL"
+fi
+if [ -z $GIT_NAME ]; then 
+    fatal_error "Must provide GIT_NAME"
+fi
+
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+
 if [ $SSH_PRIVATE_KEY_FILE ]; then
+    echo "Installing SSH key"
     mkdir -p ~/.ssh
     cp "$SSH_PRIVATE_KEY_FILE" ~/id_rsa
 
