c1eec6a605671c4af6dc60e0ac341a141064feb3
Astrid Yu <astrid@astrid.tech>
2021-09-18 20:43:35 -0700

    Add rust to line counting script

--- lc.sh
+++ lc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -type f \( -name '*.tsx' -or -name '*.ts' -or -name '*.jsx' -or -name '*.js' -or -name '*.scss' -or -name '*.py' \) \
+find . -type f \( -name '*.tsx' -or -name '*.ts' -or -name '*.jsx' -or -name '*.js' -or -name '*.scss' -or -name '*.py' -or -name '*.rs' \) \
   -not -path "./astrid_tech_frontend/node_modules/*" \
   -not -path "./astrid_tech_frontend/.next/*" \
   -not -path "./astrid_tech_frontend/out/*" \
