890a01da2306b4b70e4a83ff0e90e25d1bd59b68
Astrid Yu <astrid@astrid.tech>
2021-09-23 20:07:44 -0700

    Move Haskell into LATG server folder

--- Example.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Codec.Compression.Zlib (compress, decompress)
-import Prelude ((.), putStrLn)
-
-main = putStrLn "Hello from rules_haskell!"
-
-slowId = decompress . compress
---
+++ latg/Example.hs
@@ -0,0 +1,5 @@
+module Main where
+
+import Prelude ((.), putStrLn)
+
+main = putStrLn "Hello from rules_haskell!"
