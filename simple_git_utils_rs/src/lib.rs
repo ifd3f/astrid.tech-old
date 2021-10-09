d5a0e4dea37c6ad7ae0b8304e79eaa664a795d9d
Astrid Yu <astrid@astrid.tech>
2021-10-09 12:23:22 -0700

    sgu: Call the git commands directly

--- simple_git_utils_rs/src/lib.rs
+++ simple_git_utils_rs/src/lib.rs
@@ -1,6 +1,8 @@
-use std::{env::current_dir, error::Error, ffi::OsStr, path::PathBuf};
+use std::{error::Error, ffi::OsStr, path::PathBuf};
 
 use tokio::process::Command;
+
+type GResult<T> = Result<T, Box<dyn Error>>;
 
 pub struct ManagedGitRepo {
     repo_dir: PathBuf,
@@ -24,31 +26,54 @@ impl ManagedGitRepo {
         }
     }
 
-    pub async fn reset_dir(&mut self) -> Result<(), Box<dyn Error>> {
-        let cmd = get_script_command("reset_to_latest.sh")?.into_os_string();
-        tokio::fs::create_dir_all(&self.repo_dir).await?;
-
-        Command::new(cmd)
-            .arg(&self.remote_url)
-            .arg(&self.branch)
-            .arg(&self.base_branch)
-            .current_dir(&self.repo_dir)
-            .spawn()?
-            .wait()
-            .await?;
+    async fn git(&self, args: impl IntoIterator<Item = impl AsRef<OsStr>>) -> GResult<()> {
+        Command::new("git").args(args).current_dir(&self.repo_dir).spawn()?.wait().await?;
         Ok(())
     }
 
-    pub async fn push_changes(&mut self, message: impl AsRef<OsStr>) -> Result<(), Box<dyn Error>> {
-        let cmd = get_script_command("push_to_git.sh")?.into_os_string();
-        Command::new(&cmd)
-            .arg(message)
-            .arg(&self.remote_url)
-            .arg(&self.branch)
-            .current_dir(&self.repo_dir)
-            .spawn()?
-            .wait()
-            .await?;
+    async fn fetch(&self) -> GResult<()> {
+        self.git(["fetch", "origin", "-pP", "--depth=1", &self.base_branch]).await?;
+        self.git(["fetch", "origin", "-pP", "--depth=1", &self.branch]).await?;
+        Ok(())
+    }
+
+    pub async fn reset_dir(&mut self) -> GResult<()> {
+        // Ensure git repository exists
+        self.git(["init"]).await?;
+
+        // Clean the directory
+        self.git(["add", "-A"]).await?;
+        self.git(["reset", "--hard"]).await?;
+
+        // Ensure that origin points to the remote so we can fetch the data
+        self.git(["remote", "add", "origin", &self.remote_url]).await?;
+        self.git(["remote", "set-url", "origin", &self.remote_url]).await?;
+
+        // Fetch remote data
+        self.fetch().await?;
+
+        // In case this is our first time on this repository, check out main
+        self.git(["checkout", &self.base_branch]).await?;
+
+        // Ensure work branch exists and check it out. Deleting first ensures we 
+        self.git(["branch", "-d", &self.branch]).await?;
+        self.git(["checkout", "--guess", &self.branch]).await?;
+        self.git(["checkout", "-b", &self.branch]).await?;
+
+        Ok(())
+    }
+
+    pub async fn push_changes(&mut self, message: &str) -> Result<(), Box<dyn Error>> {
+        // Add all files and commit
+        self.git(["add", "-A"]).await?;
+        self.git(["commit", "-m", message]).await?;
+
+        self.fetch().await?;
+
+        // Push to the specified remote
+        self.git(["rebase", &format!("origin/{}", self.branch)]).await?;
+        self.git(["push"]).await?;
+
         Ok(())
     }
 }
