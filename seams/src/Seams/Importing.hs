63e4b773167098307644997d02dc5c775fb07ab1
Astrid Yu <astrid@astrid.tech>
2022-03-13 11:55:22 -0700

    seams: maybe a monad transformer stack is bad

--- seams/src/Seams/Importing.hs
+++ seams/src/Seams/Importing.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+
+
+
 module Seams.Importing where
 
 import Seams.InputSchema
@@ -14,114 +17,183 @@ import qualified Data.Map as Map
 import Data.Aeson hiding ( Result )
 import Data.Functor.Compose
 import Data.ByteString.Builder
+import Control.Applicative
+import Data.Foldable
+import Control.Monad.Trans.Reader
+import qualified Data.Bifunctor
+import Control.Monad.Trans.Class
+import Control.Monad
 
 
 newtype Error = Error String
 data WithPath a = WithPath FilePath a
 
-data FileTree a
-  = Dir { name :: String, children :: FileTree a }
-  | File { name :: String, value :: a }
+data FileTreeT m a
+  = Dir { filename :: String, ls :: m [FileTreeT m a] }
+  | File { filename :: String, value :: m a }
 
-instance Functor FileTree where
-  fmap f (Dir n cs) = Dir n (fmap f cs)
-  fmap f (File n v) = File n (f v)
+instance Functor m => Functor (FileTreeT m) where
+  fmap f (Dir n cs) = Dir n (map (f <$>) <$> cs)
+  fmap f (File n v) = File n (f <$> v)
 
-instance Foldable FileTree where
-  foldMap f (Dir n cs) = foldMap f cs
-  foldMap f (File n v) = f v
+instance Applicative m => Semigroup (FileTreeT m a) where
+  Dir dname ls <> Dir "" rs = Dir dname $ liftA2 (++) ls rs
+  Dir "" ls <> Dir dname rs = Dir dname $ liftA2 (++) ls rs
 
-type FileResult e a = FileTree (Either e a)
+  File fname val <> Dir dname cs = Dir dname ((File fname val :) <$> cs)
+  Dir dname cs <> File fname val = File fname val <> Dir dname cs
 
-readTree :: FilePath -> IO (FileResult String ByteString)
-readTree = undefined
+  l <> r = Dir "" $ pure [l, r]
 
--- | A container for data associated with importing from a specific file path.
-newtype Loader a = Loader {
-  -- | If it returns Right, then consume that file.
-  -- | If it returns Left, then redirect to a different file.
-  useFile :: FilePath -> Either (FilePath, Loader a) (ReadResult -> a)
+instance Applicative m => Monoid (FileTreeT m a) where
+  mempty = Dir "" $ pure []
+
+instance Applicative m => Applicative (FileTreeT m) where
+  pure = File "" . pure
+
+  File pf f <*> File pa a = File (pf </> pa) (f <*> a)
+  Dir n cs <*> a = Dir n (map (<*> a) <$> cs)
+  f <*> Dir n cs = Dir n (map (f <*>) <$> cs)
+
+instance Alternative m => Alternative (FileTreeT m) where
+  empty = mempty
+  (<|>) = (<>)
+
+instance Foldable m => Foldable (FileTreeT m) where
+  foldMap f (File _ a) = foldMap f a
+  foldMap f (Dir _ cs) = mconcat $ map (foldMap f) (fold cs)
+
+newtype RedirectFileT m a = RedirectFileT {
+  runFindFile :: FilePath -> m (FilePath, a)
 }
 
-runLoader loader path = case useFile loader path of
-  Right consume -> (path, consume)
-  Left (redirect, next) -> runLoader next redirect
+cd :: Applicative m => FilePath -> RedirectFileT m ()
+cd p' = RedirectFileT $ \p -> pure (p </> p', ())
+
+cd' :: Applicative m => FilePath -> RedirectFileT m ()
+cd' p = RedirectFileT $ const $ pure (p, ())
+
+pwd :: Applicative m => RedirectFileT m FilePath
+pwd = RedirectFileT $ \p -> pure (p, p)
+
+instance Functor m => Functor (RedirectFileT m) where
+  fmap f redir = RedirectFileT $ fmap (Data.Bifunctor.second f) . runFindFile redir
+
+instance Applicative m => Applicative (RedirectFileT m) where
+  pure a = RedirectFileT $ \p -> pure (p, a)
+  f <*> a = RedirectFileT $ \p ->
+    (\(pf, f') (pa, a') -> (pa, f' a')) <$> runFindFile f p <*> runFindFile a p
+
+instance Monad m => Monad (RedirectFileT m) where
+  return a = RedirectFileT $ \p -> return (p, a)
+
+  redir >>= f = RedirectFileT $ \p0 -> do
+    (p1, r1) <- runFindFile redir p0
+    runFindFile (f r1) p1
+
+instance MonadTrans RedirectFileT where
+  lift c = RedirectFileT $ \p -> do
+    result <- c
+    return (p, result)
+
+-- readFile :: ReadFileT m 
 
 type ReadError = String
-type LoaderEither e a = Compose Loader (Either e) a
 type ReadResult = Either ReadError ByteString
-type Result a = LoaderEither ReadError a
 
-toResult :: Loader a -> LoaderEither e a
-toResult a = Compose (fmap Right a)
-
-contents :: LoaderEither String ByteString
-contents = Compose $ Loader (\_ -> Right id)
-
-liftRedirect :: Either (FilePath, Loader a) (ReadResult -> a) -> Loader a
-liftRedirect r = Loader $ const r
-
-redirectOnce path consume = liftRedirect $ Left (path, consumer consume)
-
-consumer :: (ReadResult -> a) -> Loader a
-consumer f = Loader $ \_ -> Right f
-
-wrapFR :: a -> Loader a
-wrapFR a = consumer (const a)
-
-path :: Loader FilePath
-path = Loader $ Right . const
-
-instance Functor Loader where
-  fmap f (Loader makeA)
-    = Loader (\p -> case makeA p of
-                        Right consume -> Right (f . consume)
-                        Left (redirect, next) -> Left (redirect, fmap f next))
-
-instance Applicative Loader where
-  pure = wrapFR
-
-  (Loader makeF) <*> (Loader makeA)
-    = Loader (\p -> case (makeF p, makeA p) of
-                          (Right consumeF, Right consumeA) -> Right (\t -> consumeF t $ consumeA t)
-                          (f, Left (redirectA, nextA)) ->
-                            Left (redirectA, liftRedirect f <*> nextA)
-                          (Left (redirectF, nextF), a) ->
-                            Left (redirectF, nextF <*> liftRedirect a))
-
--- | Redirect to a different directory relative to current.
-cd :: FilePath -> Loader a -> Loader a
-cd p' (Loader use) = Loader $ \p -> case use p of
-  Right consume -> Left (p </> p', Loader use)
-  Left (redirect, next) -> Left (redirect </> p', next)
-
--- | cd, but on the Result level.
--- cdr :: FilePath -> Result a -> Result a
--- cdr p' ld = Compose $ Right <$> cd p' ld
-
-data Document m = Document FilePath m Content
-
-instance Functor Document where
-  fmap f (Document l m c) = Document l (f m) c
-
-data Content = Content {
-  cType :: ContentType,
-  body :: ByteString
+newtype ReadFileT m a = ReadFileT {
+  runReadFileT :: (FilePath -> m ReadResult) -> m a
 }
 
--- loadDocument :: FromJSON m => LoaderEither String (Document m)
--- loadDocument = contents <* cdr
---   where contentFilePath = dropExtension <$> path
+envReadFile :: FilePath -> ReadFileT m ReadResult
+envReadFile path = ReadFileT $ \rf -> rf path
 
--- loadContentFile :: FilePath -> LoaderEither String Content
+instance Functor m => Functor (ReadFileT m) where
+  fmap f a = ReadFileT $ fmap f . runReadFileT a
+
+instance Applicative m => Applicative (ReadFileT m) where
+  pure a = ReadFileT $ const $ pure a
+  f <*> a = ReadFileT $ \rf ->
+    runReadFileT f rf <*> runReadFileT a rf
+
+instance Monad m => Monad (ReadFileT m) where
+  return = pure
+  a >>= f = ReadFileT $ \rf ->
+    runReadFileT a rf >>= (\result -> runReadFileT (f result) rf)
+
+instance MonadTrans ReadFileT where
+  lift c = ReadFileT $ const c
+
+type TransformFileT = ReaderT ReadResult
+
+runTransformFileT :: ReaderT ReadResult m a -> ReadResult -> m a
+runTransformFileT = runReaderT
+
+fileContents :: Monad m => TransformFileT m ReadResult
+fileContents = ask
+
+type LoaderT m = RedirectFileT (TransformFileT m)
+
+-- runLoaderT :: Monad m => LoaderT m a -> FilePath -> (FilePath -> m ReadResult) -> m a
+-- runLoaderT l path readFileImpl = do
+--   (path', x) <- runFindFile l path
+--   pure $ runTransformFileT x file
+
+  -- let readFileResult = runReadFileT redirectResult
+  -- runTransformFileT readFileResult
+
+
+-- type LoaderEither m e a = Compose (Loader m) (Either e) a
+-- type Result m a = LoaderEither m ReadError a
+
+-- insertEither :: Loader m a -> LoaderEither m e a
+-- insertEither a = Compose (fmap Right a)
+-- 
+-- contents :: LoaderEither m String ByteString
+-- contents = Compose $ Loader (\_ -> Right id)
+-- 
+-- alwaysConsume :: (env ReadResult -> env a) -> Loader m a
+-- alwaysConsume c = Loader $ const (Right c)
+-- 
+-- constLoad :: a -> Loader m a
+-- constLoad a = consumer (const a)
+-- 
+-- path :: Loader m FilePath
+-- path = Loader $ Right . const
+
+-- -- | Redirect to a different directory relative to current.
+-- cd :: FilePath -> Loader a -> Loader a
+-- cd p' (Loader use) = Loader $ \p -> case use p of
+--   Right consume -> Left (p </> p', Loader use)
+--   Left (redirect, next) -> Left (redirect </> p', next)
+
+-- -- | cd, but on the Result level.
+-- cdr :: FilePath -> Result a -> Result a
+-- cdr p' ld = Compose $ cd p' (getCompose ld)
+-- 
+-- data Document m = Document FilePath m Content
+-- 
+-- instance Functor Document where
+--   fmap f (Document l m c) = Document l (f m) c
+-- 
+-- data Content = Content {
+--   cType :: ContentType,
+--   body :: ByteString
+-- }
+-- 
+-- -- loadDocument :: FromJSON m => LoaderEither String (Document m)
+-- loadDocument = cdr
+--   where contentFilePath = dropExtension <$> path
+-- 
+-- -- loadContentFile :: LoaderEither String a -> LoaderEither String Content
 -- loadContentFile path
 --   = let cType = Compose $ pure $ extensionToContentType $ takeExtension path
---         body = contents <* fmap pure cd path
+--         body = cd path
 --       in Content <$> cType <*> body
-
--- resolveContentA :: LoaderEither String UnresolvedContent -> LoaderEither String Content
-
--- parseDocument :: (FromJSON m) => FilePath -> Either String ByteString -> Either String Content -> Either String (Document m)
+-- 
+-- -- resolveContentA :: LoaderEither String UnresolvedContent -> LoaderEither String Content
+-- 
+-- -- parseDocument :: (FromJSON m) => FilePath -> Either String ByteString -> Either String Content -> Either String (Document m)
 -- parseDocument path docContent bodyContent = do
 --   dType <- extensionToDocumentType $ takeExtension path
 --   docContent' <- docContent
