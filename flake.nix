4b125d75e14a91e25118aa94cc72cf138e2db41a
Astrid Yu <astrid@astrid.tech>
2022-08-26 13:15:56 -0700

    hack to make shell work?

--- flake.nix
+++ flake.nix
@@ -15,17 +15,28 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in rec {
         packages.seams = pkgs.callPackage ./seams { };
+
         devShells = {
           default = import ./shell.nix { inherit pkgs; };
+
           seams = pkgs.haskellPackages.shellFor {
             packages = p: [ packages.seams ];
             withHoogle = true;
+            # postgresql-typed and dbmate params
+            TPG_DB = "seams-db";
+            TPG_USER = "postgres";
+            TPG_PASS = "developer";
+            TPG_HOST = "localhost";
+            TPG_PORT = 5432;
             buildInputs = with pkgs; [
               haskellPackages.haskell-language-server
+              postgresql
               cabal2nix
               nixfmt
               haskellPackages.hindent
               haskellPackages.hlint
+              haskellPackages.cabal-install
+              zlib
             ];
           };
         };
--- seams/default.nix
+++ seams/default.nix
@@ -1,6 +1,6 @@
 { haskell }:
 let
-  hps = haskell.packages.ghc922.extend (final: prev: rec {
+  hps = haskell.packages.ghc923.extend (final: prev: rec {
    seams-cms = final.callCabal2nix "seams" ./. { };
  });
 in
