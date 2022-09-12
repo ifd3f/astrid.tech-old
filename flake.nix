447ca07f63639bcd8132bb3360c71e87e23d560b
Astrid Yu <astrid@astrid.tech>
2022-09-12 09:18:17 -0700

    Inline the flake shell

--- flake.nix
+++ flake.nix
@@ -6,11 +6,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { self, flake-utils, nixpkgs, ... }@inputs:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" ] (system: {
-      devShell = import ./shell.nix {
-        pkgs = nixpkgs.legacyPackages.${system};
-      };
-    });
+  outputs = { self, flake-utils, nixpkgs, ... }@inputs:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+    ] (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cargo
+            curl
+            docker
+            docker-compose
+            git
+            nodejs
+            pipenv
+            python310
+            rustc
+            yarn
+          ];
+        };
+      });
 }
